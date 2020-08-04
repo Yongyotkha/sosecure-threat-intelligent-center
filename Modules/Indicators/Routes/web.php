8747b4249b6bb3ca6b83d5fd3f3d1c598c83560a
mtsccoth <yongyot.kha@mtsc.co.th>
2020-08-04 14:53:30 +0700

    no message

---
+++ Modules/Indicators/Routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+
+Route::group(
+    ['middleware' => 'web', 'prefix' => 'indicators'],
+    function () {
+        Route::get('/', 'IndicatorsController@index')->name('indicators.index')->middleware('can:menu_items');
+    }
+);
