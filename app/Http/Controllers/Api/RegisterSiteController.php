85b3239296689af0832f59a95b305aa9a1c74fb5
S4NTOD3 <todeooooo@gmail.com>
2020-10-30 12:17:30 +0700

    การ install site

--- app/Http/Controllers/Api/RegisterSiteController.php
+++ app/Http/Controllers/Api/RegisterSiteController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Modules\SiteSettings\Entities\SiteSettings;
+use App\file_version;
 
 class RegisterSiteController extends Controller
 {
@@ -30,7 +31,29 @@ class RegisterSiteController extends Controller
     }
 
     public function test_get(Request $request){
-        return response()->json(['message' => 'Successful', 'error' => '', 'status_code' => '200', 'data' => $request -> page]);
+        $ip = '192.168.2.1';
+        $mac = 'fe80::8c98:dba3:69f3:2ecb%6';
+        $value = '{
+            "system_version" : "0.0.0",
+            "last_update" : "2020-10-21 13:11:33",
+            "main" : 1
+        }';
+        $data = $this->encrypt_decrypt('encrypt', $value, $ip, $mac);
+        return response()->json(['message' => 'Successful', 'error' => '', 'status_code' => '200', 'data' => $data]);
+    }
+
+    public function site_request_version(Request $request){
+        $ip = '192.168.2.1';
+        $mac = 'fe80::8c98:dba3:69f3:2ecb%6';
+        $value = $request -> key;
+        $data = $this->encrypt_decrypt('decrypt', $value, $ip, $mac);
+        if($data === false){
+            return response()->json(['error' => 'The request parameters are invalid', 'status_code' => '400']);
+        }else{
+            $data_version = json_decode($data, true);
+            $file_version = file_version::where('main', 1)->orderBy('created_at', 'desc')->first();
+            return response()->json(['message' => 'Successful', 'error' => '', 'status_code' => '200', 'data' => ['new_version' => $file_version , 'current_version' => $data_version]]);
+        }
     }
 
     private function encrypt_decrypt($action, $string, $ip, $mac) {
