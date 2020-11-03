edbf21e0cf4df0783392e30a3b265b536027a1df
S4NTOD3 <todeooooo@gmail.com>
2020-11-03 18:26:13 +0700

    no message

--- Modules/Scans/Routes/web.php
+++ Modules/Scans/Routes/web.php
@@ -14,6 +14,7 @@ Route::group(
     ['middleware' => 'web', 'prefix' => 'scans'],
     function () {
         Route::get('/', 'ScansController@index')->name('scans.index')->middleware('can:menu_items');
-        Route::get('/scans-domain/{tab?}', 'ScansController@scan_domain')->name('scans.index');
+        Route::get('/scans-domain/{tab}', 'ScansController@scan_domain')->name('scans.index');
+        Route::get('/scan_command', 'ScansController@scan_command');
     }
 );
