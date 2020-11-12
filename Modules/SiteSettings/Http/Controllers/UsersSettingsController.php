4b0f435dee94593bdf9ce42985ec3b82ed4a0f04
arsarawut <arsarawut.pa@mtsc.co.th>
2020-11-12 09:23:04 +0700

    no message

--- Modules/SiteSettings/Http/Controllers/UsersSettingsController.php
+++ Modules/SiteSettings/Http/Controllers/UsersSettingsController.php
@@ -21,6 +21,7 @@ class UsersSettingsController extends Controller
      */
     protected $item;
     protected $siteSettings;
+    protected $user;
     /**
      * Request instance
      *
@@ -28,11 +29,12 @@ class UsersSettingsController extends Controller
      */
     protected $request;
 
-    public function __construct(Request $request, SiteSettings $siteSettings)
+    public function __construct(Request $request, SiteSettings $siteSettings, User $user)
     {
         $this->middleware(['auth', 'verified', '2fa']);
         $this->request = $request;
         $this->siteSettings = $siteSettings;
+        $this->user = $user;
     }
     /**
      * Display a listing of the resource.
@@ -139,25 +141,25 @@ class UsersSettingsController extends Controller
     {
         // dd($request);
         // exit();
-        $domain = $this->domain->findOrFail($id);
-        // $domain->update($request->all());
-        $domain->name = $request->name;
-        $domain->domain = $request->domain;
-        // $domain->open_scan = $request->open_scan;
-        // $domain->scan_interval = $request->scan_interval;
-        $domain->status = $request->status ? 1 : 0;
-        $domain->save();
+        $user = $this->user->findOrFail($id);
+        // $user->update($request->all());
+        $user->name = $request->name;
+        $user->user = $request->user;
+        // $user->open_scan = $request->open_scan;
+        // $user->scan_interval = $request->scan_interval;
+        $user->status = $request->status ? 1 : 0;
+        $user->save();
 
-        $site_code = $this->siteSettings->find_code($domain->site_id);
+        $site_code = $this->siteSettings->find_code($user->site_id);
 
         // if ($request->hasFile('logo')) {
-        //     $this->uploadLogo($request, $domain);
+        //     $this->uploadLogo($request, $user);
         // }
         return ajaxResponse(
             [
-                'id'       => $domain->id,
+                'id'       => $user->id,
                 'message'  => langapp('changes_saved_successful'),
-                'redirect' => route('domain.index',['id' => $site_code->code]),
+                'redirect' => route('userssettings.index',['id' => $site_code->code]),
             ],
             true,
             Response::HTTP_OK
@@ -166,7 +168,9 @@ class UsersSettingsController extends Controller
 
     public function delete_process($id = null)
     {
-        $model = $this->domain->find($id);
+        // $data['user'] = User::where('code', $id)->first();
+        // $model = $this->user->find($id);
+        $model = $this->user->where('code',$id)->first();
         // dd($model);
         $model->delete();
 
@@ -175,7 +179,7 @@ class UsersSettingsController extends Controller
         return ajaxResponse(
             [
                 'message'  => langapp('deleted_successfully'),
-                'redirect' => route('domain.index',['id' => $site_code->code]),
+                'redirect' => route('userssettings.index',['id' => $site_code->code]),
             ],
             true,
             Response::HTTP_OK
@@ -185,17 +189,17 @@ class UsersSettingsController extends Controller
 
     public function change_status(Request $request)
     {
-        $Domain = Domain::where('code', $request->code)->first();
-        $Domain->status = $request->active;
-        $Domain->save();
+        $user = User::where('code', $request->code)->first();
+        $user->active = $request->active;
+        $user->save();
 
-        $site_code = $this->siteSettings->find_code($Domain->site_id);
+        $site_code = $this->siteSettings->find_code($user->site_id);
 
         return ajaxResponse(
             [
-                'id'       => $Domain->id,
+                'id'       => $user->id,
                 'message'  => langapp('changes_saved_successful'),
-                'redirect' => route('domain.index',['id' => $site_code->code]),
+                'redirect' => route('userssettings.index',['id' => $site_code->code]),
             ],
             true,
             Response::HTTP_OK
@@ -216,7 +220,7 @@ class UsersSettingsController extends Controller
     }
 
     public function test() {
-        $model = $this->domain->query();
+        $model = $this->user->query();
         // dd($model);
         // return $modal;
        dd(DataTables::eloquent($model)->make(true));
@@ -225,38 +229,44 @@ class UsersSettingsController extends Controller
     public function tableData()
     {
         // $model = $this->applyFilter()->with(['profile:user_id,job_title,mobile,city,use_gravatar,avatar']);
-        $model = $this->domain->query();
+        $model = $this->user->query();
         // var_dump($model);
         // exit();
         return DataTables::eloquent($model)
             ->editColumn(
                 'no',
-                function ($domain) {
-                    return $domain->id;
+                function ($user) {
+                    return $user->id;
                 }
             )
             ->editColumn(
                 'chk',
-                function ($domain) {
-                    return '<label><input type="checkbox" name="checked" value="' . $domain->id . '"><span class="label-text"></span></label>';
+                function ($user) {
+                    return '<label><input type="checkbox" name="checked" value="' . $user->id . '"><span class="label-text"></span></label>';
                 }
             )
             ->editColumn(
                 'name',
-                function ($domain) {
-                    return $domain->name;
+                function ($user) {
+                    return $user->name;
                 }
             )
             ->editColumn(
-                'domain',
-                function ($domain) {
-                    return $domain->domain;
+                'email',
+                function ($user) {
+                    return $user->email;
+                }
+            )
+            ->editColumn(
+                'role',
+                function ($user) {
+                    return $user->user;
                 }
             )
             ->editColumn(
                 'status',
-                function ($domain) {
-                    if($domain->status == '1') {
+                function ($user) {
+                    if($user->active == '1') {
                         $checked_val = 'checked';
                     } else {
                         $checked_val = '';
@@ -265,7 +275,7 @@ class UsersSettingsController extends Controller
                     
                     // $html = '';
                     $html .= '<label class="switch">
-                                <input type="checkbox" id="domain_active_'.$domain->code.'" onchange="change_domain_active(\''. $domain->code .'\')" '.$checked_val.' name="active" value="1">
+                                <input type="checkbox" id="user_active_'.$user->code.'" onchange="change_user_active(\''. $user->code .'\')" '.$checked_val.' name="active" value="1">
                                 <span></span>
                               </label>';
 
@@ -274,36 +284,37 @@ class UsersSettingsController extends Controller
             )
             ->editColumn(
                 'action',
-                function ($domain) {
+                function ($user) {
                     $html = '';
                     $html .= "<!--<a href='' class='btn btn-". get_option('theme_color') ." btn-xs' data-toggle='ajaxModal'>
                                     @icon('solid/shield-alt')
                                 </a>-->
                                 
-                                <a href='". route('domainsettings.edit', ['id' => $domain->id]) ."' class='btn btn-". get_option('theme_color') ." btn-xs' data-toggle='ajaxModal'>
+                                <a href='". route('user.edit', ['id' => $user->code]) ."' class='btn btn-". get_option('theme_color') ." btn-xs' data-toggle='ajaxModal'>
                                 <svg class='svg-inline--fa' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z'></path></svg>
                                 </a>
-                                <a href='". route('domainsettings.delete', ['id' => $domain->id]) ."' class='btn btn-". get_option('theme_color') ." btn-xs' data-toggle='ajaxModal'>
+                                <a href='". route('user.delete2', ['id' => $user->code]) ."' class='btn btn-". get_option('theme_color') ." btn-xs' data-toggle='ajaxModal'>
                                 <svg class='svg-inline--fa' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'><path d='M0 84V56c0-13.3 10.7-24 24-24h112l9.4-18.7c4-8.2 12.3-13.3 21.4-13.3h114.3c9.1 0 17.4 5.1 21.5 13.3L312 32h112c13.3 0 24 10.7 24 24v28c0 6.6-5.4 12-12 12H12C5.4 96 0 90.6 0 84zm416 56v324c0 26.5-21.5 48-48 48H80c-26.5 0-48-21.5-48-48V140c0-6.6 5.4-12 12-12h360c6.6 0 12 5.4 12 12zm-272 68c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208z'></path></svg>
                                 </a>";
                     return $html;
                 }
             )
-            ->rawColumns(['no', 'chk', 'name', 'domain', 'status', 'action'])
+            ->rawColumns(['no', 'chk', 'name', 'email', 'role', 'status', 'action'])
             ->make(true);
     }
 
 
-    public function edit(Domain $id)
+    public function edit(User $id)
     {
-        $data['domain'] = $id;
+        $data['user'] = $id;
         // dd($id);
-        return view('sitesettings::modal.update_domain')->with($data);
+        return view('sitesettings::modal.update_user')->with($data);
     }
 
-    public function delete(Domain $id)//del_domain
+    public function delete(Request $request, $id)//del_domain
     {
-        $data['domain'] = $id;
-        return view('sitesettings::modal.delete_domain')->with($data);
+        $data['user'] = User::where('code', $id)->first();
+        // $data['user'] = $id;
+        return view('sitesettings::modal.delete_user')->with($data);
     }
 }
