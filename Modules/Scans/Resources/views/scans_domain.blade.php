497623a9c0e965d5a974026cc353185957583144
jamesicky1@gmail.com <jamesicky1@gmail.com>
2020-11-17 18:11:59 +0700

    no message

--- Modules/Scans/Resources/views/scans_domain.blade.php
+++ Modules/Scans/Resources/views/scans_domain.blade.php
@@ -13,6 +13,9 @@
                 @if($tab == 'datatype')
                     <div class="button-control pull-right">
                         <div class="btn-group">
+                            <button class="btn btn-{{ get_option('theme_color') }} btn-sm dropdown-toggle" id="asset-to-use" disabled> Asset To Use</button>
+                        </div>
+                        <div class="btn-group">
                             <button class="btn btn-{{ get_option('theme_color') }} btn-sm dropdown-toggle" id="fillter-advance"> @langapp('filter') Advance</button>
                         </div>
                         <div class="btn-group">
