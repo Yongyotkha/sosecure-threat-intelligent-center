ffccfa1b5dcad2fc2925cfe8beafd5b4b73f5990
arsarawut <arsarawut.pa@mtsc.co.th>
2020-11-17 17:14:46 +0700

    no message

--- Modules/Scans/Http/Controllers/ScansController.php
+++ Modules/Scans/Http/Controllers/ScansController.php
@@ -223,8 +223,7 @@ class ScansController extends Controller
             })
             ->addColumn('action', function (TransactionTimeStampScans $model) {
                 $html = '';
-                $html .= "
-                            <a href='". route('scans.index', ['tab' => 'overview', 'site_code' => $model -> code]) ."' class='btn btn-". get_option('theme_color') ." btn-xs'>
+                $html .= "<div style='display: flex;'><a href='". route('scans.index', ['tab' => 'overview', 'site_code' => $model -> code]) ."' class='btn btn-". get_option('theme_color') ." btn-xs'>
                                 <i class='far fa-eye'></i>
                             </a>
                             <a href='' class='btn btn-". get_option('theme_color') ." btn-xs' data-toggle='ajaxModal'>
@@ -235,7 +234,7 @@ class ScansController extends Controller
                             </a>
                             <a href='' class='btn btn-". get_option('theme_color') ." btn-xs' data-toggle='ajaxModal'>
                             <svg class='svg-inline--fa' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'><path d='M0 84V56c0-13.3 10.7-24 24-24h112l9.4-18.7c4-8.2 12.3-13.3 21.4-13.3h114.3c9.1 0 17.4 5.1 21.5 13.3L312 32h112c13.3 0 24 10.7 24 24v28c0 6.6-5.4 12-12 12H12C5.4 96 0 90.6 0 84zm416 56v324c0 26.5-21.5 48-48 48H80c-26.5 0-48-21.5-48-48V140c0-6.6 5.4-12 12-12h360c6.6 0 12 5.4 12 12zm-272 68c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208z'></path></svg>
-                            </a>";
+                            </a></div>";
                 return $html;
             })
             ->rawColumns(['chk','name','domain','progress','action'])
