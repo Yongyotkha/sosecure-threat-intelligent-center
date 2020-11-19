8bdd74fd0541bfb09124382e39a686ac52dfd290
arsarawut <arsarawut.pa@mtsc.co.th>
2020-11-19 16:18:50 +0700

    no message

--- Modules/Scans/Http/Controllers/ScansController.php
+++ Modules/Scans/Http/Controllers/ScansController.php
@@ -314,12 +314,14 @@ class ScansController extends Controller
 
     public function tableData()
     {
+        $site_id = '';
         $site_code = $this->request->site_code;
         // $site_code = 'a7b6ff37-30ec-4494-9527-93b0ccc51d56';
         $site_id_find = SiteSettings::where("code",$site_code)->first();
         if($site_code) {
             $site_id = $site_id_find->id;
         }
+        
 
         // $site_code = $this->request->site_code;
         // $site_id = $this->request->site_id;
