4ddea70f44ae62ca2b64137d0f77dea63c41cf10
arsarawut <arsarawut.pa@mtsc.co.th>
2020-11-19 14:45:52 +0700

    no message

--- resources/views/emails/create_site_user.blade.php
+++ resources/views/emails/create_site_user.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 {{-- Share Workice with friend --}}
 Hello, there is a register using this email.   
-Please press to confirm the application at the link. <a hred="{{route('reauth.verify_site_user',['token' => $summary['site_add_user_token']])}}">{{route('reauth.verify_site_user',['token' => $summary['site_add_user_token']])}}</a> นี้ หรือ  
+Please press to confirm the application at the link. <a href="{{route('reauth.verify_site_user',['token' => $summary['site_add_user_token']])}}">{{route('reauth.verify_site_user',['token' => $summary['site_add_user_token']])}}</a> นี้ หรือ  
 @component('mail::button', ['url' => route('reauth.verify_site_user',['token' => $summary['site_add_user_token']]), 'color' => 'blue'])
 Verify Registrer
 @endcomponent
