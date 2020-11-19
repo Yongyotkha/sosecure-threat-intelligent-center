f9e152cbbdce053604b06dc9ad64fe9997750b29
arsarawut <arsarawut.pa@mtsc.co.th>
2020-11-19 15:24:20 +0700

    no message

--- Modules/SiteSettings/Resources/views/domain.blade.php
+++ Modules/SiteSettings/Resources/views/domain.blade.php
@@ -183,7 +183,8 @@
                 url: '{!! route('scans.data') !!}',
                 data: {
                     "site_code":'{{ Request::segment(3) }}'
-                }
+                },
+                type: "POST",
             },
             order: [
                 [0, "desc"]
@@ -305,7 +306,7 @@
             toastr.success(response.data.message, '@langapp('response_status')');
             window.location.href = response.data.redirect;
         }).catch(function (error) {
-            var errors = error.errors;
+            var errors = error.response.data.errors;
             var errorsHtml = "";
             $.each(errors, function (key, value) {
                 errorsHtml += "<li>" + value[0] + "</li>";
