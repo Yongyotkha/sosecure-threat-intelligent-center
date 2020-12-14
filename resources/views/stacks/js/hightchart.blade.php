332f09db0963aa12c5da6f4691f989bad4bd031f
jamexaoj <jamesicky1@gmail.com>
2020-12-14 15:11:55 +0700

    no message

---
+++ resources/views/stacks/js/hightchart.blade.php
@@ -0,0 +1,6 @@
+<script src="{{ getAsset('plugins/Highcharts-Stock/code/js/highstock.js') }}"></script>
+<script src="{{ getAsset('plugins/Highcharts-Stock/code/js/data.js') }}"></script>
+<script src="{{ getAsset('plugins/Highcharts-Stock/code/js/drilldown.js') }}"></script>
+<script src="{{ getAsset('plugins/Highcharts-Stock/code/js/exporting.js') }}"></script>
+<script src="{{ getAsset('plugins/Highcharts-Stock/code/js/export-data.js') }}"></script>
+<script src="{{ getAsset('plugins/Highcharts-Stock/code/accessibility.js') }}"></script>
