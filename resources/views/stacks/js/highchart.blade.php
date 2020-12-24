fd1f3284777f0d4cc0b86423f6f60331457ae76e
jamexaoj <jamesicky1@gmail.com>
2020-12-24 17:41:56 +0700

    no message

--- resources/views/stacks/js/highchart.blade.php
+++ resources/views/stacks/js/highchart.blade.php
@@ -1,6 +1,6 @@
 <script src="{{ getAsset('plugins/Highcharts-Stock/code/highstock.js') }}"></script>
 <script src="{{ getAsset('plugins/Highcharts-Stock/code/data.js') }}"></script>
 <script src="{{ getAsset('plugins/Highcharts-Stock/code/drilldown.js') }}"></script>
-<script src="{{ getAsset('plugins/Highcharts-Stock/code/exporting.js') }}"></script>
-<script src="{{ getAsset('plugins/Highcharts-Stock/code/export-data.js') }}"></script>
+{{-- <script src="{{ getAsset('plugins/Highcharts-Stock/code/exporting.js') }}"></script>
+<script src="{{ getAsset('plugins/Highcharts-Stock/code/export-data.js') }}"></script> --}}
 <script src="{{ getAsset('plugins/Highcharts-Stock/code/accessibility.js') }}"></script>
