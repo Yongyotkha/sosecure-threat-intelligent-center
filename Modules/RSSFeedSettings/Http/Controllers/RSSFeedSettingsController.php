5e8e2e8913997d109ae4f95d778fbd292e93db20
arsarawut <arsarawut.pa@mtsc.co.th>
2020-12-27 13:21:55 +0700

    no message

--- Modules/RSSFeedSettings/Http/Controllers/RSSFeedSettingsController.php
+++ Modules/RSSFeedSettings/Http/Controllers/RSSFeedSettingsController.php
@@ -436,7 +436,13 @@ class RSSFeedSettingsController extends Controller
                 return $html;
             })
             ->addColumn('link', function (RSSNews $model) {
-                return '<a href="#">TH</a> | <a href="#">EN</a>';
+                $html = '';
+                $html_th = '<a href="'.route('news.public_detail_select', ['code' => $model->code , 'lang' => 'th']).'" target="_blank">TH</a>';
+                $html_en = '<a href="'.route('news.public_detail_select', ['code' => $model->code , 'lang' => 'en']).'" target="_blank">EN</a>';
+
+                $html .= $html_th .' | '. $html_en;
+
+                return $html;
             })
             ->addColumn('status', function (RSSNews $model) {
                 if($model->status == '1') {
