fcdad3f6bd10f26eb1dd188e8fa865543be0d7c5
jamexaoj <jamesicky1@gmail.com>
2021-01-04 08:56:13 +0700

    no message

--- resources/views/partial/top_header.blade.php
+++ resources/views/partial/top_header.blade.php
@@ -5,20 +5,20 @@
                 @icon('solid/bars')
             </a>
             <a href="{{  url('/')  }}" class="navbar-brand">
-                <img src="{{asset('images/logo_threat/logo.png')}}" class="m-r-sm" onerror="setDefaultPic(this)">
+                <img src="{{asset('images/logo_threat/logo_site.png')}}" class="m-r-sm" onerror="setDefaultPic(this)">
                 @php $display = get_option('logo_or_icon'); @endphp
                 {{-- @if ($display == 'logo' || $display == 'logo_title')
                 <img src="{{ getStorageUrl(config('system.media_dir').'/'.get_option('company_logo'))  }}" class="m-r-sm" onerror="setDefaultPic(this)">
                 @elseif ($display == 'icon' || $display == 'icon_title')
                 <i class="fa {{  get_option('site_icon')  }}"></i>
                 @endif --}}
-                @if ($display == 'logo_title' || $display == 'icon_title')
+                {{-- @if ($display == 'logo_title' || $display == 'icon_title')
                 @if (get_option('website_name') == '')
                 {{ get_option('company_name') }}
                 @else
                 {{ get_option('website_name') }}
                 @endif
-                @endif
+                @endif --}}
             </a>
             <a class="btn btn-link visible-xs" data-toggle="dropdown" data-target=".nav-user">
                 @icon('solid/cog')
