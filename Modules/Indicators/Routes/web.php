62c73288a0d58c844e6a7279cdf6381385229c88
armkung505 <phitchayakorn.cpe@gmail.com>
2021-01-06 13:35:55 +0700

    no message

--- Modules/Indicators/Routes/web.php
+++ Modules/Indicators/Routes/web.php
@@ -21,8 +21,7 @@ Route::group(
         Route::get('/events', 'IndicatorsController@events')->name('indicators.events')->middleware('can:menu_items');
         Route::get('/events/events_detail', 'IndicatorsController@events_detail')->name('indicators.events_detail')->middleware('can:menu_items');
         Route::get('/events/events_detail/{id}', 'IndicatorsController@events_detail_select')->name('indicators.events_detail_select')->middleware('can:menu_items');
-        Route::get('/group/{id}', 'IndicatorsController@link_group')->name('indicators.link_group')->middleware('can:menu_items');
-        Route::get('/tags/{id}', 'IndicatorsController@link_tags')->name('indicators.link_tags')->middleware('can:menu_items');
+       
 
         Route::get('/attributes', 'IndicatorsController@attributes')->name('indicators.attributes')->middleware('can:menu_items');
         // Route::get('/details', 'IndicatorsController@show_detail_indicators')->name('indicators.detail_indicators')->middleware('can:menu_items');
@@ -36,5 +35,12 @@ Route::group(
         Route::post('events_pulse_table', 'IndicatorsController@load_pulse_tb')->name('indicators.events_pulse_table')->middleware('can:menu_items');
         Route::post('events_table_search', 'IndicatorsController@datatableEvent_search')->name('indicators.events_table_search')->middleware('can:menu_items');
         Route::get('/count_view_event', 'IndicatorsController@count_view')->name('indicators.count_view_event');
+
+        Route::get('/groups/{id}', 'IndicatorsController@link_group')->name('indicators.link_group')->middleware('can:menu_items');
+        Route::post('/groups/table_groups', 'IndicatorsController@table_groups')->name('indicators.table_groups')->middleware('can:menu_items');
+
+        Route::get('/tags/{id}', 'IndicatorsController@link_tags')->name('indicators.link_tags')->middleware('can:menu_items');
+        Route::post('/tags/table_tags', 'IndicatorsController@table_tags')->name('indicators.table_tags')->middleware('can:menu_items');
+
     }
 );
