1917ce6ca4aa6064f61d23353ce7011cf66f8fa3
jamexaoj <jamesicky1@gmail.com>
2021-01-07 12:09:16 +0700

    no message

--- Modules/DarkWeb/Resources/views/index.blade.php
+++ Modules/DarkWeb/Resources/views/index.blade.php
@@ -48,21 +48,10 @@
                                         <option value="" >All</option>
                                         <option value="compromise" >Public</option>
                                         <option value="darkweb" >Darkweb</option>
-                                        {{-- <option value="1" selected>All</option> --}}
                                     </select>
                                 </div>
                             </div>
                         </div>
-                        {{-- <div class="col-lg-4">
-                            <div class="row d-flex align-items-center">
-                                <label for="" class="col-sm-3 col-xs-12 col-form-label">Sources</label>
-                                <div class="col-sm-9 col-xs-12">
-                                    <section id="sources_select" class="select2-option form-control">
-                                        <option value="1" selected>All</option>
-                                    </section>
-                                </div>
-                            </div>
-                        </div> --}}
                         <div class="col-lg-4 text-center">
                             <div id="newsrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; display:block;margin-bottom:0;">
                                 <i class="fa fa-calendar"></i>&nbsp;
@@ -87,106 +76,52 @@
                             </button>
                         </div>
                     </div>
-                </div>
-                {{-- <div class="container-fluid" style="padding: 2rem;">
-                    <div class="row m-b-md">
-                        <div class="col-lg-4">
-                            <label for="">Select Source</label>
-                            <select id="social" class="select2-option form-control">
-                                <option value="" selected>All</option>
-                            </select>
-
-                        </div>
-                        <div class="col-lg-8">
-                            <label for="">Keywords</label>
-                            <input type="text" id="Keywords" class="form-control">
-                        </div>
-                    </div>
-
-
-                    <div class="row">
-                        <div class="col-lg-12 text-right">
-                            <button id="btn_news_search" class="btn btn-info btn-responsive">
-                                <i class="fas fa-search"></i>
-                                Search
-                            </button>
-                            <button class="btn btn-default btn-responsive" style="white-space: nowrap">
-                                <i class="fas fa-broom"></i>
-                                <span> Clear </span>
-                            </button>
-                        </div>
-                    </div>
-                </div> --}}
-
-
-
-                
+                </div>              
             </section>
 
 
-{{--             
-            <div class="col-md-12">
-                <h3 class="text-dark">Display 300 record</h3>
-            </div>
-            <div class="row m-b-md">
-                <div class="col-sm-12">
-                    <div class="shadow-box-news b-d-all">
-                        <article class="def-rlt">
-                            <div class="entry">
-                                <span class="entry-category">
-                                    <a href="#">http://gjobqjj7wyczbqie.onion/</a>
-                                </span>
-                                <h3>
-                                    <a href="#">
-                                        WhatsApp’s new fact-check feature lets users identify fake information
-                                    </a>
-                                </h3>
-                                <div class="entry-meta">
-                                    <span class="entry-date"> <i class="fas fa-calendar-alt"></i> August 4th, 2020</span>
-                                    <span class="entry-view"> <i class="fas fa-eye"></i> 300</span>
+            <section class="panel panel-default">
+                <div class="panel-body">
+                    <div class="col-md-4">
+                        <div class="card-dash">
+                            <div class="left-card">
+                                <div class="img-icon-card">
+                                    <img src="{{asset('images/compromise.png')}}" alt="">
                                 </div>
-                                <div class="description-text hidden-xs">
-                                    WhatsApp's "Search the Web" feature lets users perform web searches on viral messages to confirm their authenticity.
-                                </div>
+                                <h3 class="name-dash-text text-dark text-upper ">Public</h3>
+                                <span class="number-card info">5</span>
                             </div>
-                        </article>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card-dash">
+                            <div class="left-card">
+                                <div class="img-icon-card">
+                                    <img src="{{asset('images/compromise.png')}}" alt="">
+                                </div>
+                                <h3 class="name-dash-text text-dark text-upper">Dark Web</h3>
+                                <span class="number-card info">5</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card-dash">
+                            <div class="left-card">
+                                <div class="img-icon-card">
+                                    <img src="{{asset('images/webserver.png')}}" alt="">
+                                </div>
+                                <h3 class="name-dash-text text-dark text-upper ">Web Server</h3>
+                                <span class="number-card info">5</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-sm-12">
-                    <div class="shadow-box-news b-d-all">
-                        <article class="def-rlt">
-                            <div class="entry">
-                                <span class="entry-category">
-                                    <a href="#">http://gjobqjj7wyczbqie.onion/</a>
-                                </span>
-                                <h3>
-                                    <a href="#">
-                                        How hackers behind Twitter Bitcoin scam were caught
-                                    </a>
-                                </h3>
-                                <div class="entry-meta">
-                                    <span class="entry-date"> <i class="fas fa-calendar-alt"></i> August 4th, 2020</span>
-                                    <span class="entry-view"> <i class="fas fa-eye"></i> 300</span>
-                                </div>
-                                <div class="description-text hidden-xs">
-                                    WhatsApp's "Search the Web" feature lets users perform web searches on viral messages to confirm their authenticity.
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                </div>
-            </div> --}}
-            
+            </section>
 
             <div class="tabbable">
                 <ul class="nav nav-tabs nav-tabs-highlight">
                     <li class="active"><a href="#tab-leak" class="tab_cliick" data-toggle="tab">Data (<span id="count_news"></span>)</a></li>
                     <li  id="tab-bookmark"><a href="#tab-bookmarks" class="tab_cliick" data-toggle="tab">My Bookmarks (<span id="count_news_bookmark"></span>)</a></li>   
-                    <li class="pull-right">
-                        {{-- <button id="" class="btn btn-sm btn-{{ get_option('theme_color')  }} pull-right">
-                            <span>Bookmarks</span>
-                         </button> --}}
-                    </li>
                 </ul>
                 <div class="tab-content">
 
@@ -196,91 +131,23 @@
                             <div id="main-list" class="row m-b-md">
                                 <div class="col-md-12">
                                     <div id="list_news"></div>
-                                    <div id="load_list_news" class="ajax-loading loading-more" style="display: none;margin-top:15px;">Loading&nbsp;<span class="content-spinner-loading-inline"></span></div>
-                                        {{-- <div class="list-news">
-                                            <div class="checkbox-news-select">
-                                                <label class="mr-3">
-                                                    <input type="checkbox" name="" class="chk-bookmark">
-                                                    <span class="label-text checkbox-news-input"></span>
-                                                </label>
-                                            </div>
-                                            <div class="content-news-text">
-                                                <a href="{{route('news.news_detail')}}">
-                                                    <a href="{{route('news.news_detail')}}">
-                                                    <span class="head-news-text">WhatsApp’s new fact-check feature lets users identify fake information</span>
-                                                </a>
-                                                </a>
-                                                <div class="entry-meta">
-                                                    <span class="entry-date"> <i class="fas fa-calendar-alt"></i> August 4th, 2020</span>
-                                                    <span class="entry-view"> <i class="fas fa-eye"></i> 300</span>
-                                                    <span>&nbsp;Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptates delectus praesentium architecto iure reprehenderit soluta qui sapiente quaerat, explicabo non mollitia officiis sit porro consequuntur itaque, iusto ad quas.</span>
-                                                </div>
-                                            </div>
-                                            <div class="content-news-image">
-                                                <a href="{{route('news.news_detail')}}">
-                                                    <img src="https://images.unsplash.com/photo-1602524207251-8ad5d2b2c303?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="">
-                                                </a>
-                                            </div>
-                                            <div class="action-bookmark">
-                                                <i class="fas fa-bookmark" id="mark1" onclick="Bookmarks(this)"></i>
-                                            </div>
-                                        </div> --}}
-                                    
-                                    
+                                    <div id="load_list_news" class="ajax-loading loading-more" style="display: none;margin-top:15px;">Loading&nbsp;<span class="content-spinner-loading-inline"></span></div>                                  
                                 </div>
                             </div>
                         </section>
                     </div>
-
-
-
                     <div class="tab-pane" id="tab-bookmarks">
                         <section class="panel panel-default border-n">
                             <div class="row m-b-md">
                                 <div class="col-sm-12">
-                                    
                                     <div id="list_news_book_mark"></div>
                                     <div id="load_list_news_book_mark" class="ajax-loading loading-more" style="display: none;margin-top:15px;">Loading&nbsp;<span class="content-spinner-loading-inline"></span></div>
-                                    {{-- <div class="list-news">
-                                        <div class="checkbox-news-select">
-                                            <label class="mr-3">
-                                                <input type="checkbox" name="" class="chk-bookmark">
-                                                <span class="label-text checkbox-news-input"></span>
-                                            </label>
-                                        </div>
-                                        <div class="content-news-text">
-                                            <a href="{{route('news.news_detail')}}">
-                                                <a href="{{route('news.news_detail')}}">
-                                                <span class="head-news-text">WhatsApp’s new fact-check feature lets users identify fake information</span>
-                                            </a>
-                                            </a>
-                                            <div class="entry-meta">
-                                                <span class="entry-date"> <i class="fas fa-calendar-alt"></i> August 4th, 2020</span>
-                                                <span class="entry-view"> <i class="fas fa-eye"></i> 300</span>
-                                                <span>&nbsp;Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptates delectus praesentium architecto iure reprehenderit soluta qui sapiente quaerat, explicabo non mollitia officiis sit porro consequuntur itaque, iusto ad quas.</span>
-                                            </div>
-                                        </div>
-                                        <div class="content-news-image">
-                                            <a href="{{route('news.news_detail')}}">
-                                                <img src="https://images.unsplash.com/photo-1602524207251-8ad5d2b2c303?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="">
-                                            </a>
-                                        </div>
-                                        <div class="action-bookmark">
-                                            <i class="fas fa-bookmark" id="mark1" onclick="Bookmarks(this)"></i>
-                                        </div>
-                                    </div> --}}
-
-
                                 </div>
                             </div>
                         </section>  
                     </div>
-
-
                 </div>
-
             </div>
-
           </section>
     </section>
     <a href="#" class="hide nav-off-screen-block" data-toggle="class:nav-off-screen" data-target="#nav"></a>
