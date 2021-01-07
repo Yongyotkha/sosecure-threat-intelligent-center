ca47823417ba60d5544bf36ab829da7d9abea24a
armkung505 <phitchayakorn.cpe@gmail.com>
2021-01-07 18:51:26 +0700

    no message

--- Modules/WebDefacement/Entities/WebdefacmentSetting.php
+++ Modules/WebDefacement/Entities/WebdefacmentSetting.php
@@ -5,6 +5,9 @@ namespace Modules\WebDefacement\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Modules\SiteSettings\Entities\SiteSettings;
 use Modules\WebDefacement\Entities\WebdefacmentDataOriginal;
+use Modules\WebDefacement\Entities\WebdefacmentDataCheck;
+use Modules\WebDefacement\Entities\WebdefacmentDataLog;
+
 
 class WebdefacmentSetting extends Model
 {
@@ -15,6 +18,23 @@ class WebdefacmentSetting extends Model
     {
         return $this->hasOne(SiteSettings::class, 'id', 'site_id');
     }
+
+    public function get_webdefacment_data_original()
+    {
+        return $this->hasMany(WebdefacmentDataOriginal::class, 'webdefacment_setting_id', 'id');
+    }
+
+    public function get_webdefacment_data_check()
+    {
+        return $this->hasMany(WebdefacmentDataCheck::class, 'webdefacment_setting_id', 'id');
+    }
+
+    public function get_webdefacment_data_log()
+    {
+        return $this->hasMany(WebdefacmentDataLog::class, 'webdefacment_setting_id', 'id');
+    }
+
+    
 
     public function webdefacment_data_original_last($webdefacment_setting_id)
     {
