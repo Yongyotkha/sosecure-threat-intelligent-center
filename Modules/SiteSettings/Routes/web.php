3958f036b128f035daf1d2ce63fb3e5c787fe43e
arsarawut <arsarawut.pa@mtsc.co.th>
2021-01-08 11:14:54 +0700

    no message

--- Modules/SiteSettings/Routes/web.php
+++ Modules/SiteSettings/Routes/web.php
@@ -111,6 +111,7 @@ Route::group(
 
 Route::get('/WebDefacement-website/{id}', 'WebDefacementController@webdefacement_website')->name('webdefacement_website.index')->middleware('can:menu_items');
 Route::post('/WebDefacement-website/load_card_by_site', 'WebDefacementController@load_card_by_site')->name('webdefacement.load_card_by_site')->middleware('can:menu_items');
+Route::post('/WebDefacement-website/get_check_site', 'WebDefacementController@get_check_site')->name('webdefacement.get_check_site')->middleware('can:menu_items');
 Route::get('/edit-image', 'WebDefacementController@edit_image')->name('webdefacement_website.edit_image')->middleware('can:menu_items');
 Route::get('/WebDefacement-server/{id}', 'WebDefacementController@webdefacement_server')->name('webdefacement_server.index')->middleware('can:menu_items');
 
