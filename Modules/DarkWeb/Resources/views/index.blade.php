1a9ea1b16dbac50da497b6cdb3310981d4220578
jamexaoj <jamesicky1@gmail.com>
2021-01-09 15:07:37 +0700

    no message

--- Modules/DarkWeb/Resources/views/index.blade.php
+++ Modules/DarkWeb/Resources/views/index.blade.php
@@ -86,9 +86,9 @@
                         <div class="card-dash none-bg none-shadow">
                             <div class="left-card">
                                 <div class="img-icon-card ice">
-                                    <img src="{{asset('images/iceberg1.png')}}" alt="">
+                                    <img src="{{asset('images/iceberg2.png')}}" alt="">
                                 </div>
-                                <h3 class="name-dash-text text-white text-upper ">Public</h3>
+                                <h3 class="name-dash-text text-dark text-upper ">Public</h3>
                                 <span class="number-card warning">135</span>
                             </div>
                         </div>
@@ -97,9 +97,9 @@
                         <div class="card-dash none-bg none-shadow">
                             <div class="left-card">
                                 <div class="img-icon-card ice">
-                                    <img src="{{asset('images/iceberg2.png')}}" alt="">
+                                    <img src="{{asset('images/iceberg1.png')}}" alt="">
                                 </div>
-                                <h3 class="name-dash-text text-white text-upper">Dark Web</h3>
+                                <h3 class="name-dash-text text-dark text-upper">Dark Web</h3>
                                 <span class="number-card info">105</span>
                             </div>
                         </div>
@@ -110,7 +110,7 @@
                                 <div class="img-icon-card ice">
                                     <img src="{{asset('images/webserver.png')}}" alt="">
                                 </div>
-                                <h3 class="name-dash-text text-white text-upper ">Web Server</h3>
+                                <h3 class="name-dash-text text-dark text-upper ">Web Server</h3>
                                 <span class="number-card green">500</span>
                             </div>
                         </div>
