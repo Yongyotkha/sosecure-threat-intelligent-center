3627b44cb722e1d2d016bbe8bb633a57059ab471
S4NTOD3 <todeooooo@gmail.com>
2021-01-12 19:56:51 +0700

    no message

--- app/Http/Controllers/Api/RegisterSiteController.php
+++ app/Http/Controllers/Api/RegisterSiteController.php
@@ -25,12 +25,12 @@ class RegisterSiteController extends ApiController
             return response()->json(['error' => 'The request parameters are invalid', 'status_code' => '400']);
         }else{
             $data_key = json_decode($data, true);
-            $data_key_decrypt = $this->encrypt_decrypt('decrypt', $data_key['key'], $site['data']['ip_key'],  $site['data']['mac_address_key']);
-            if($data_key_decrypt === false){
-                return response()->json(['error' => 'The request parameters are invalid', 'status_code' => '400']);
-            }else{
-                $site_explode = explode('&', $data_key_decrypt);
-                $site = SiteSettings::where('code', $site_explode[0])->first();
+            // $data_key_decrypt = $this->encrypt_decrypt('decrypt', $data_key['key'], $site['data']['ip_key'],  $site['data']['mac_address_key']);
+            // if($data_key_decrypt === false){
+            //     return response()->json(['error' => 'The request parameters are invalid', 'status_code' => '400', 'data' => $data_key['key']]);
+            // }else{
+            //     $site_explode = explode('&', $data_key_decrypt);
+                $site = SiteSettings::where('code', $site['data']['code'])->first();
                 if($site->no_expiration_active === 0){
                     return response()->json(['message' => 'Successful', 'error' => '', 'status_code' => '200', 'data' => $site]);
                 }else if($site->no_expiration_active === 1 && ($site->start_active_key <= date("Y-m-d H:i:s") && $site->end_active_key >= date("Y-m-d H:i:s"))){
@@ -38,7 +38,7 @@ class RegisterSiteController extends ApiController
                 }else{
                     return response()->json(['error' => 'The key is invalid', 'status_code' => '401']);
                 }
-            }
+            // }
         }
     }
 
@@ -57,7 +57,7 @@ class RegisterSiteController extends ApiController
             $data_key = json_decode($data, true);
             $data_key_decrypt = $this->encrypt_decrypt('decrypt', $data_key['key'], $site['data']['ip_key'],  $site['data']['mac_address_key']);
             if($data_key_decrypt === false){
-                return response()->json(['error' => 'The request parameters are invalid', 'status_code' => '400']);
+                return response()->json(['error' => 'The request parameters are invalid', 'status_code' => '400', 'data' => $data_key]);
             }else{
                 $new_version = DeployCode::select('version', 'created_at')->where('status', 1)->where('access_type', 1)->orderBy('version', 'desc')->first();
                 $current_version = DeployCode::select('version', 'created_at')->where('version', $data_key_decrypt)->first();
