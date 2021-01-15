3d24bbc3b6dad977511accbb1def7b21be39e2c8
jamexaoj <jamesicky1@gmail.com>
2021-01-15 16:41:44 +0700

    no message

--- Modules/SiteSettings/Resources/views/compromised_web_server.blade.php
+++ Modules/SiteSettings/Resources/views/compromised_web_server.blade.php
@@ -161,7 +161,7 @@
 
 <div class="modal in fixed-left" id="add_asset_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
     aria-hidden="true">
-    <div class="modal-dialog modal-dialog-aside" role="document"></div>
+    <div class="modal-dialog modal-dialog-aside" role="document">
         <div class="modal-content">
 
             <div class="modal-header bg-blue">
