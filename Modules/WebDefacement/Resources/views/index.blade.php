f1c54eb563d115d3c724784c729f18d90b00864c
jamexaoj <jamesicky1@gmail.com>
2021-01-15 14:01:28 +0700

    no message

--- Modules/WebDefacement/Resources/views/index.blade.php
+++ Modules/WebDefacement/Resources/views/index.blade.php
@@ -7,7 +7,7 @@
             <div class="bc-head"> @langapp('webdefacement')</div>    
 
             <button id="advance-search" href="#area-advance-search" class="btn btn-sm btn-{{ get_option('theme_color')  }} pull-right">
-                <span>@langapp('Search_Advance')</span>
+                <span><i class="fas fa-filter"></i> @langapp('Search_Advance')</span>
              </button>
              <div class="pull-right" style="margin-top: 8px; width: 300px;">
                 <select name="site" id="site" class="select2-option form-control select-site" style="min-width: 300px">
