10afb16ed175a3f202dbe6848f1b28aa127e33de
arsarawut <arsarawut.pa@mtsc.co.th>
2021-01-15 14:23:40 +0700

    no message

--- Modules/RSSFeedSettings/Http/Controllers/RSSFeedSettingsController.php
+++ Modules/RSSFeedSettings/Http/Controllers/RSSFeedSettingsController.php
@@ -1501,7 +1501,7 @@ class RSSFeedSettingsController extends Controller
     public function rss_news()
     {
         // $data['page'] = langapp('rss_logs');
-        $data['page'] = 'news';
+        $data['page'] = langapp('news');
         // $data['Category'] = CategorySettings::where('active',1)->get();
         $data['category'] = CategorySettings::where('active',1)->get();
         return view('rssfeedsettings::rss_news')->with($data);
