13a1a0cc18dabccf2f9995fd6376935e9e2da81f
armkung505 <phitchayakorn.cpe@gmail.com>
2021-01-19 09:20:53 +0700

    no message

--- Modules/Scans/Resources/views/scans_domain.blade.php
+++ Modules/Scans/Resources/views/scans_domain.blade.php
@@ -94,7 +94,7 @@
                         <button data-target="#asset_to_use_manual" data-toggle="modal" class="btn btn-{{ get_option('theme_color') }} btn-sm dropdown-toggle" id="asset-to-use-manual">Add Asset To Use</button>
                     </div>
                     <div class="btn-group">
-                        <button class="btn btn-{{ get_option('theme_color') }} btn-sm dropdown-toggle" id="fillter-advance">@langapp('Search_Advance')</button>
+                        <button class="btn btn-{{ get_option('theme_color') }} btn-sm dropdown-toggle" id="fillter-advance"><i class="fas fa-filter"></i> @langapp('Search_Advance')</button>
                     </div>
 
                 </div>
