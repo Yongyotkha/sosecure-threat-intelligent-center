d458fb84e55f0fc4373c4cbe300c5ca785686b96
arsarawut <arsarawut.pa@mtsc.co.th>
2021-01-21 18:02:43 +0700

    no message

--- Modules/SiteSettings/Http/Controllers/DataLeakController.php
+++ Modules/SiteSettings/Http/Controllers/DataLeakController.php
@@ -637,6 +637,15 @@ class DataLeakController extends Controller
                 // });
             }
 
+            if ($request->type) {
+
+                $type = $request->type;
+                $model->whereHas('get_data_leak_feed_one', function ($query) use ($type) {
+                    $query->where('feel_type', 'LIKE', '%' . $type . '%');
+                });
+
+            }
+
             if ($request->source) {
 
                 $source = $request->source;
@@ -1325,7 +1334,7 @@ class DataLeakController extends Controller
                 'type',
                 function ($model) {
                     if ($model->get_data_leak_feed_temp_one) {
-                        return $model->get_data_leak_feed_temp_one->feed_type;
+                        return get_word_leak_compromise($model->get_data_leak_feed_temp_one->feed_type,'compromise');
                     } else {
                         return '-';
                     }
