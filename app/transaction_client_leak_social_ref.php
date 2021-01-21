6d2594e5bcf25d59d9c851eb556c6812cae4e28c
S4NTOD3 <todeooooo@gmail.com>
2021-01-21 14:14:25 +0700

    no message

---
+++ app/transaction_client_leak_social_ref.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class transaction_client_leak_social_ref extends Model
+{
+    protected $table = 'transaction_client_leak_social_ref';
+}
