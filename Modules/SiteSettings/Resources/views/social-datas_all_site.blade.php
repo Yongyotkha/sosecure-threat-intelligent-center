400d7095796bab44ac4bbafea7881c09558c13f6
armkung505 <phitchayakorn.cpe@gmail.com>
2021-01-21 11:17:32 +0700

    no message

--- Modules/SiteSettings/Resources/views/social-datas_all_site.blade.php
+++ Modules/SiteSettings/Resources/views/social-datas_all_site.blade.php
@@ -590,7 +590,7 @@
                         width: '10px',
                         render: function (data, type, full, meta) {
       
-                            return `<a href="${base_url}/socialdatas/delete_dataleakdata_modal/${full.code}" class="btn btn-{{get_option("theme_color")}} btn-xs" data-toggle="ajaxModal"><i class="fas fa-trash-alt"></i></a>`;
+                            return `<a href="${base_url}/socialdatas/delete_dataleakdata_modal/${full.code}" class="btn btn-danger btn-xs" data-toggle="ajaxModal"><i class="fas fa-trash-alt"></i></a>`;
                       
                             
         
