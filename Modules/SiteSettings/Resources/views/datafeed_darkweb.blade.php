b4a848f88a6540b056a919c4227b36587ee871a7
armkung505 <phitchayakorn.cpe@gmail.com>
2021-01-22 15:12:51 +0700

    no message

--- Modules/SiteSettings/Resources/views/datafeed_darkweb.blade.php
+++ Modules/SiteSettings/Resources/views/datafeed_darkweb.blade.php
@@ -746,50 +746,7 @@ function change_status(){
 }
 
 
-    $("#btn_del_select").click(function() {
-        $('.val_id:checked').each(function () {
-            val_id.push(this.value);
-            
-        });
 
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            heightAuto: false,
-            confirmButtonText: 'Yes!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                $.ajax({
-                    type:"POST",
-                    url:"{{ route('darkweb.delete_select_process') }}",
-                    data:{id: val_id},
-                    beforeSend: function(){
-                        loading('load');
-                    },
-                    success:function(response) {
-                        loading('stop_load');
-                        toastr.success(response.message, '@langapp('response_status')');
-                        window.location.href = response.redirect;
-                    },
-                    error: function (error){
-                        loading('stop_load');
-                        var errors = error.response.data.errors;
-                        var errorsHtml = '';
-                        $.each(errors, function (key, value) {
-                            errorsHtml += '<li>' + value[0] + '</li>';
-                        });
-                        toastr.error(errorsHtml, '@langapp('response_status') ');
-                    }
-                
-                });
-
-            }
-        })
-    });
 
 
 </script>
