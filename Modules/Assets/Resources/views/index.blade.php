2b4a91c78e5d732375fb58967f6dfb80497188b9
jamexaoj <jamesicky1@gmail.com>
2021-01-27 16:07:44 +0700

    no message

--- Modules/Assets/Resources/views/index.blade.php
+++ Modules/Assets/Resources/views/index.blade.php
@@ -248,8 +248,6 @@
                                                 <span class="label-text"></span>
                                             </label>
                                         </th> --}}
-
-                                        <!-- ปล. เมื่อมีการเพิ่ม ให้ rowspan ++ -->
                                         <th rowspan="2" class="align-middle">Site</th>
                                         <th rowspan="2" class="align-middle">Domain</th>
                                         <th rowspan="2" class="align-middle">IP</th>
@@ -268,10 +266,11 @@
                                 </thead>
                                 <tbody>
                    
+                                    <!-- ปล. เมื่อมีการเพิ่ม ให้ rowspan ++ -->
                                     <tr>
-                                        <td rowspan="2">บริษัท เมจิก</td>
-                                        <td rowspan="2">mtsc.co.th</td>
-                                        <td rowspan="2">104.24.14.205</td>
+                                        <td rowspan="4">บริษัท เมจิก</td>
+                                        <td rowspan="4">mtsc.co.th</td>
+                                        <td rowspan="4">104.24.14.205</td>
                                         <td>Microsoft</td>
                                         <td>Windows_10</td>
                                         <td>r2</td>
@@ -287,6 +286,38 @@
                                         </td>
                                     </tr>
 
+                                    <tr>
+                                        <!-- ตัวอย่างเวลา rowspan แถวต่อไป ไม่ต้องเอาค่า 3 อันแรกมาแสดง ถ้าเพิ่มใน rowspan ทำแบบนี้ไปเรื่อยๆ แล้วให้ rowspan ด้านบน ++ -->    
+                                        <td>Microsoft</td>
+                                        <td>Windows_10</td>
+                                        <td>r2</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>
+                                            <a href="#" class="btn btn-xs btn-danger"><i class="fas fa-trash"></i></a>
+                                        </td>
+                                        <td><span class="badge badge-success">Active</span></td>
+                                        <td>
+                                            <a href="#" class="btn btn-xs btn-info"><i class="fas fa-edit"></i></a>
+                                            <a href="#" class="btn btn-xs btn-info"><i class="fas fa-plus"></i> Add</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <!-- ตัวอย่างเวลา rowspan แถวต่อไป ไม่ต้องเอาค่า 3 อันแรกมาแสดง ถ้าเพิ่มใน rowspan ทำแบบนี้ไปเรื่อยๆ แล้วให้ rowspan ด้านบน ++ -->    
+                                        <td>Microsoft</td>
+                                        <td>Windows_10</td>
+                                        <td>r2</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>
+                                            <a href="#" class="btn btn-xs btn-danger"><i class="fas fa-trash"></i></a>
+                                        </td>
+                                        <td><span class="badge badge-success">Active</span></td>
+                                        <td>
+                                            <a href="#" class="btn btn-xs btn-info"><i class="fas fa-edit"></i></a>
+                                            <a href="#" class="btn btn-xs btn-info"><i class="fas fa-plus"></i> Add</a>
+                                        </td>
+                                    </tr>
                                     <tr>
                                         <!-- ตัวอย่างเวลา rowspan แถวต่อไป ไม่ต้องเอาค่า 3 อันแรกมาแสดง ถ้าเพิ่มใน rowspan ทำแบบนี้ไปเรื่อยๆ แล้วให้ rowspan ด้านบน ++ -->    
                                         <td>Microsoft</td>
