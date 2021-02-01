2f38bb0e482350bb0302d059e9e1537d9a738ed3
arsarawut <arsarawut.pa@mtsc.co.th>
2021-02-01 11:01:01 +0700

    no message

--- Modules/SiteSettings/Http/Controllers/UsersSettingsController.php
+++ Modules/SiteSettings/Http/Controllers/UsersSettingsController.php
@@ -764,7 +764,7 @@ class UsersSettingsController extends Controller
         // $site_code = $this->request->site_code;
         // $site_id = $this->request->site_id;
         // $model = $this->applyFilter()->with(['profile:user_id,job_title,mobile,city,use_gravatar,avatar']);
-        $model = User::where('deleted_at', null)->where('active',1)->where('site_id', $site_id)->orderBy('id','ASC');
+        $model = User::where('deleted_at', null)->where('site_id', $site_id)->orderBy('id','ASC');
 
         $model->whereHas('get_model_has_roles',function($q) {
             $q->whereIn('role_id',[4,5,6]);
