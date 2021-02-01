2a67a37b0da4c286ae67194261590319bc764cc4
arsarawut <arsarawut.pa@mtsc.co.th>
2021-02-01 10:47:10 +0700

    no message

--- Modules/SiteSettings/Resources/views/modal/update_user.blade.php
+++ Modules/SiteSettings/Resources/views/modal/update_user.blade.php
@@ -144,7 +144,7 @@
                     .then(function (response) {
                             toastr.success(response.data.message, '@langapp('response_status') ');
                             $(form_save).html('<i class="fas fa-check"></i> @langapp('save') </span>');
-                            {{--window.location.href = response.data.redirect;--}}
+                            window.location.href = response.data.redirect;
                 })
                 .catch(function (error) {
                     if(error.response.data.exception){
