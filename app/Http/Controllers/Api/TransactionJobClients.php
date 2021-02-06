b22f1c8f8cac4e8daeacb47d83304759dea16ef3
S4NTOD3 <todeooooo@gmail.com>
2021-02-06 16:13:32 +0700

    no message

--- app/Http/Controllers/Api/TransactionJobClients.php
+++ app/Http/Controllers/Api/TransactionJobClients.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\transcation_jobs_clients;
 use App\Entities\TF_Center_transaction_batchjob;
+use App\Log;
+
 class TransactionJobClients extends ApiController
 {
     public function transaction_job_clients(Request $request){
@@ -95,6 +97,41 @@ class TransactionJobClients extends ApiController
                     $datas = encrypt_decrypt('encrypt', $data_transcation_jobs_clients, $header, $data['site']['data']['ip_key'],  $data['site']['data']['mac_address_key']);
                     return response()->json(['message' => 'Successful', 'error' => '', 'status_code' => '200', 'data' => $datas]);
 
+                } catch (\Exception $e) {
+                    $response = array(
+                        'status_code' => 500,
+                        'message' => $e -> getMessage(),
+                    );
+                    return response()->json($response);
+                }
+            }
+        } catch (\Exception $e) {
+            $response = array(
+                'status_code' => 500,
+                'message' => $e -> getMessage(),
+            );
+            return response()->json($response);
+        }
+    }
+
+    public function tranferDB(Request $request){
+        try{
+            $header = $request->bearerToken();
+            $mode = $request->mode;
+            $data_request = $request -> data;
+            $data = $this -> dataFalse($header, $mode, $data_request);
+            if($data === false){
+                return response()->json(['error' => 'The request parameters are invalid', 'status_code' => '400']);
+            }else{
+                try {
+                    $log = new Log;
+                    $log -> site_id = $data['site']['data']['id'];
+                    $log -> file = $data['data']['return_data'];
+                    $log -> error_summary = $data['data']['error_summary'];
+                    $log -> log_trace = $data['data']['log_trace'];
+                    $log -> save();
+                    
+                    return response()->json(['message' => 'Successful', 'error' => '', 'status_code' => '200']);
                 } catch (\Exception $e) {
                     $response = array(
                         'status_code' => 500,
