aa206297c4763faf9a7049db349085106c1f4c1d
mtsccoth <yongyot.kha@mtsc.co.th>
2021-02-06 15:54:52 +0700

    no message

--- app/Entities/Transaction_user_site.php
+++ app/Entities/Transaction_user_site.php
@@ -8,7 +8,7 @@ use App\Entities\User_site;
 
 class Transaction_user_site extends Model
 {
-    protected $table = 'transaction_user_site';
+    protected $table = 'transaction_client_users_site';
     protected $primaryKey = 'id';
     public $incrementing = true;
     protected $dates   = ['created_at', 'updated_at'];
