481b1e03aa22e884402c7f02c3c99b7d416e01a1
arsarawut <arsarawut.pa@mtsc.co.th>
2021-02-09 16:12:17 +0700

    no message

--- resources/views/partial/header-select-site.blade.php
+++ resources/views/partial/header-select-site.blade.php
@@ -1,7 +1,17 @@
 <header class="dk header b-b" style="display: flex;align-items: center;"> 
     <div class="select-box-header">
         <select name="site" id="site" class="text-left select2-option form-control select-site"style="min-width:191px">
-            <option value="" selected="selected">{{ @$siteSettings->name }}</option>
+            <option value=""></option>
+            @if(Modules\SiteSettings\Entities\SiteSettings::select('id','code','name')->where('active',1)->where('deleted_at',null)->get())
+                @foreach (Modules\SiteSettings\Entities\SiteSettings::select('id','code','name')->where('active',1)->where('deleted_at',null)->get() as $SiteSettings)
+                    @if($SiteSettings->code == @$siteSettings->code)
+                        <option value="{{ $SiteSettings->code }}" selected>{{ $SiteSettings->name }}</option>
+                    @else
+                        <option value="{{ $SiteSettings->code }}">{{ $SiteSettings->name }}</option>
+                    @endif
+                @endforeach
+            @endif
+
         </select>
     </div>
     <a class="hide-setting menu-site hide-xs-menu btn btn-icon btn-default btn-sm m-r-xs">@icon('solid/bars')</a>
