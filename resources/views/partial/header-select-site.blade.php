745183707c43a0ce393208a44de09a6d6ebebdca
arsarawut <arsarawut.pa@mtsc.co.th>
2021-02-09 16:39:41 +0700

    no message

--- resources/views/partial/header-select-site.blade.php
+++ resources/views/partial/header-select-site.blade.php
@@ -1,7 +1,7 @@
 <header class="dk header b-b" style="display: flex;align-items: center;"> 
     <div class="select-box-header">
-        <select name="site" id="site" class="text-left select2-option form-control select-site"style="min-width:191px">
-            <option value=""></option>
+        <select name="site" id="site_in_siteSetting" class="text-left select2-option form-control select-site"style="min-width:191px">
+            {{-- <option value="">All Site</option> --}}
             @if(Modules\SiteSettings\Entities\SiteSettings::select('id','code','name')->where('active',1)->where('deleted_at',null)->get())
                 @foreach (Modules\SiteSettings\Entities\SiteSettings::select('id','code','name')->where('active',1)->where('deleted_at',null)->get() as $SiteSettings)
                     @if($SiteSettings->code == @$siteSettings->code)
@@ -17,3 +17,13 @@
     <a class="hide-setting menu-site hide-xs-menu btn btn-icon btn-default btn-sm m-r-xs">@icon('solid/bars')</a>
     <a class="btn btn-icon menu-site btn-default btn-sm m-r-xs visible-xs" data-toggle="class:show" data-target="#setting-nav">@icon('solid/bars')</a>
 </header>
+
+@push('pagescript')
+    <script>
+
+        $("#site_in_siteSetting").change(function() {
+            let site_in_siteSetting = $(this).val();
+            window.location.href = base_url+'/sitesettings/edit-sitesettings/'+site_in_siteSetting;
+        });
+    </script>
+@endpush
