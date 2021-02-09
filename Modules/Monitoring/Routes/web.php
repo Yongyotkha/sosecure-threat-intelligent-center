b3005615ae48db8532c6ff3c8ad1b802431bfcd9
arsarawut <arsarawut.pa@mtsc.co.th>
2021-02-09 17:04:37 +0700

    no message

--- Modules/Monitoring/Routes/web.php
+++ Modules/Monitoring/Routes/web.php
@@ -18,5 +18,8 @@ Route::group(
         Route::post('/tableMonitor', 'MonitoringController@tableMonitor')->name('monitoring.tableMonitor')->middleware('can:menu_items');
 
         Route::get('/view_message/{id}', 'MonitoringController@view_message_modal')->name('monitoring.view_message_modal')->middleware('can:menu_items');
+
+        Route::get('/monitor_logs', 'MonitoringController@monitor_logs')->name('monitoring.monitor_logs')->middleware('can:menu_items');
+        Route::post('/table_monitor_logs', 'MonitoringController@table_monitor_logs')->name('monitoring.table_monitor_logs')->middleware('can:menu_items');
     }
 );
