e7e8f657ed78056bb72c3d404b94f5548a49f7d3
nontawat_pm <codenon@hotmail.com>
2021-02-11 14:11:52 +0700

    no message

--- Modules/DarkWeb/Http/Controllers/DarkWebController.php
+++ Modules/DarkWeb/Http/Controllers/DarkWebController.php
@@ -625,13 +625,14 @@ class DarkWebController extends Controller
                 }
             }
 
-            $news = $news->with('get_data_leak_feed_one')->orderBy('id','desc')->paginate(PAGINATE_NUM);
+            
 
             if($site_id) {
                 $news = $news->where('site_id', $site_id);
                 $countGroupBy = $countGroupBy->where('site_id', $site_id);
             }
-            // $countGroupBy = $countGroupBy->get();
+            $news = $news->with('get_data_leak_feed_one')->orderBy('id','desc')->paginate(PAGINATE_NUM);
+            $countGroupBy = $countGroupBy->get();
 
 
         }
