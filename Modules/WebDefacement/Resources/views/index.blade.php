f1ffe8d50fc86acffd3caadcd31c024b9b787b8f
jamexaoj <jamesicky1@gmail.com>
2021-02-11 11:59:47 +0700

    no message

--- Modules/WebDefacement/Resources/views/index.blade.php
+++ Modules/WebDefacement/Resources/views/index.blade.php
@@ -13,7 +13,7 @@
                 </div>
 
                 <div class="ml-2 text-right">
-                    <div class="text-left" style="min-width:270px;display:inline-block;">
+                    <div class="text-left max-w-select" style="display:inline-block;">
                         <select name="site" id="site" class="select2-option form-control select-site">
                             <option value="">All Site</option>
                             @if($SiteSettings)
