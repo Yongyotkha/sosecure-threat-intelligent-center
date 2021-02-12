7816a73daa8fde5d2b894678de84321c34ef632d
jamexaoj <jamesicky1@gmail.com>
2021-02-12 09:21:18 +0700

    no message

--- Modules/SiteSettings/Resources/views/darkweb-datas_all_site.blade.php
+++ Modules/SiteSettings/Resources/views/darkweb-datas_all_site.blade.php
@@ -625,7 +625,7 @@
                                 }
                                 
                             }
-                            return '<div class="scroll-ovf" data-rel="tooltip" title="'+feedcontent+'">'+content+'</div>';
+                            return '<textarea class="resize-none" rows="3" cols="50" disabled="">'.content.'</textarea>';
                         },
                     },
 
@@ -642,7 +642,7 @@
                                 }
                             }
         
-                            return '<div class="scroll-ovf" data-rel="tooltip" title="'+val+'">'+val+'</div>';
+                            return '<textarea class="resize-none" rows="3" cols="50" disabled="">'.val.'</textarea>';
 
                         },
                     
