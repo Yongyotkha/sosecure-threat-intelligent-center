349c43754896f73608ddd13784e1796eb197a672
jamexaoj <jamesicky1@gmail.com>
2021-02-13 11:24:32 +0700

    no message

--- Modules/News/Resources/views/news_detail.blade.php
+++ Modules/News/Resources/views/news_detail.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.app')
 @section('image','{{@$RSSNews->logo}}')
-{{-- @section('image','https://images.unsplash.com/photo-1597086657068-7e10f874e8c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=986&q=80') --}}
 @section('content')
 <section id="content" class="bg">
-    {{-- @php(dd(@$RSSNews->get_topic->topic->name)) --}}
-    {{-- @php(dd(@$RSSNews->get_topic_multi)) --}}
     <section class="hbox stretch">
         <section class="vbox">
             {{-- Head --}}
@@ -25,10 +22,11 @@
                 <div class="bc-head">@langapp('news') > {{@$RSSNews->title_th}}</div>
 
                 @if($RSSNews_next)
-                <a href="{{route('news.news_detail_code',['code' => @$RSSNews_next->code])}}"><button
-                        class="btn btn-sm btn-{{ get_option('theme_color')  }} pull-right" style="margin-top: 10px;">
+                <a href="{{route('news.news_detail_code',['code' => @$RSSNews_next->code])}}">
+                    <button class="btn btn-sm btn-{{ get_option('theme_color')  }} pull-right" style="margin-top: 10px;">
                         <span><i class="fas fa-arrow-right"></i></span>
-                    </button></a>
+                    </button>
+                </a>
                 @else
                 <button class="btn btn-sm btn-{{ get_option('theme_color')  }} pull-right" disabled>
                     <span><i class="fas fa-arrow-right"></i></span>
@@ -49,7 +47,7 @@
                     <span><i class="fas fa-print"></i></span>
                 </button>
             </header>
-            
+
             <section class="scrollable wrapper">
                 <div class="section-jumborton">
                     {{-- <div class="thumnail-img" style="background-image:url('https://images.unsplash.com/photo-1597086657068-7e10f874e8c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=986&q=80')"></div> --}}
@@ -117,30 +115,6 @@
                             </div>
                             <div class="show-content-news" id="content_news">
                                 {!!@$RSSNews->detail_th!!}
-    
-                                {{-- <p>Intel is currently looking into how 20GB of sensitive internal data came to find its way online.</p>
-    
-                                <p>The range of documents — some marked “confidential,” “under NDA” or “restricted secret”— were uploaded to file hosting service MEGA by Swiss Android developer Till Kottmann.</p>
-                                
-                                <p>Before his account was suspended <a href="https://twitter.com/deletescape/status/1291405688204402689" target="_blank" data-feathr-click-track="true">by Twitter</a>, Kottmann explained on the site that “most of the things here have not been published anywhere before.”</p>
-                                
-                                <p>They include details on chip roadmaps, development and debugging tools, schematics, training videos, process simulator ADKs, sample code, Bringup guides and much more.</p>
-                                
-                                <p>Affected platforms include Kaby Lake, Snow Ridge, Elkhart Lake and the unreleased 10nm Tiger Lake architecture.</p>
-                                
-                                <p>Kottmann claimed to have received this data from a third party who found it on an unsecured server via a simple nmap scan. Many of the zip files were reportedly protected with easy-to-guess or crack passwords.</p>
-                                
-                                <p>However, Intel doesn’t believe the data came from a network breach, and said in a brief statement that it is urgently investigating what may have happened.</p>
-                                
-                                <p>“The information appears to come from the Intel Resource and Design Center, which hosts information for use by our customers, partners and other external parties who have registered for access,” it continued. “We believe an individual with access downloaded and shared this data."</p>
-                                
-                                <p>Although there appears to have been no personally identifiable information (PII) exposed in the breach, the compromise of so many sensitive internal documents will be ringing alarm bells at the chipmaker’s HQ — especially as more leaks have been promised.</p>
-                                
-                                <p>Erich Kron, security awareness advocate at KnowBe4, said the incident highlights supply chain cyber-risk.</p>
-                                
-                                <p>“There is always a risk when sharing potentially sensitive information to these business partners, however, this is often an unavoidable part of doing business,” he added.</p>
-                                
-                                <p>“Whenever providing intellectual property access to another organization or individual, it is important to log not only who has access, but when and what data they are accessing. Even better, as in this case with Intel, ensuring that you know where the documents have been shared by potentially marking the document itself, can be very valuable when hunting potential misuse as appears to have occurred here."</p> --}}
                             </div>
                         </div>
                         <div class="col-md-4">
@@ -150,26 +124,20 @@
                                     <h2>Related to This Story</h2>
                                 </li>
                                 @if(@$RSSNews_last10)
-                                @if($lang == 'th')
-                                @foreach($RSSNews_last10 as $RSSNews_last10_val)
-                                <li><a
-                                        href="{{route('news.news_detail_code',['code' => @$RSSNews_last10_val->code])}}">{{@$RSSNews_last10_val->title_th}}</a>
-                                </li>
-                                @endforeach
-                                @else
-                                @foreach($RSSNews_last10 as $RSSNews_last10_val)
-                                <li><a
-                                        href="{{route('news.news_detail_code',['code' => @$RSSNews_last10_val->code])}}">{{@$RSSNews_last10_val->title_en}}</a>
-                                </li>
-                                @endforeach
+                                    @if($lang == 'th')
+                                        @foreach($RSSNews_last10 as $RSSNews_last10_val)
+                                        <li><a
+                                                href="{{route('news.news_detail_code',['code' => @$RSSNews_last10_val->code])}}">{{@$RSSNews_last10_val->title_th}}</a>
+                                        </li>
+                                        @endforeach
+                                    @else
+                                    @foreach($RSSNews_last10 as $RSSNews_last10_val)
+                                        <li><a
+                                                href="{{route('news.news_detail_code',['code' => @$RSSNews_last10_val->code])}}">{{@$RSSNews_last10_val->title_en}}</a>
+                                        </li>
+                                    @endforeach
+                                     @endif
                                 @endif
-                                @endif
-                                {{-- <li><a href="">Lorem ipsum dolor, sit amet consectetur Lorem ipsum dolor, sit amet consectetur </a></li>
-                                <li><a href="">adipisicing elit. Autem quis cum veniam Autem quis cum veniam</a> </li>
-                                <li><a href="">laudantium expedita hic illum optio expedita hic illum</a></li>
-                                <li><a href="">laudantium expedita hic illum optio expedita hic illum</a></li>
-                                <li><a href="">eius alias. Eum id odit pariatur, reprehenderit  odit pariatur, reprehenderit</a></li>
-                                <li><a href="">eius alias. Eum id odit pariatur, reprehenderit  odit pariatur, reprehenderit</a></li> --}}
                             </ul>
                             @endif
                         </div>
