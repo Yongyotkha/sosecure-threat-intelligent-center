b759ddb1c18cf85db131b63fc49caa8e6c248dbb
jamexaoj <jamesicky1@gmail.com>
2021-02-15 13:43:34 +0700

    no message

--- Modules/WebsiteSalePage/Resources/views/index.blade.php
+++ Modules/WebsiteSalePage/Resources/views/index.blade.php
@@ -40,12 +40,12 @@
 
     </div>
     <div class="move-down">
-        <a href="#"><i class="fa fa-angle-down text-white fa-2x"></i></a>
+        <a href="#start-section"><i class="fa fa-angle-down text-white fa-2x"></i></a>
     </div>
   </section><!-- #intro -->
 
   <main id="main">
-    <section class="content-section">
+    <section id="start-section" class="content-section">
         <div class="container">
             <div class="row d-flex align-items-center">
                 <div class="col-lg-6 col-md-6">
@@ -124,14 +124,14 @@
         <div class="iceberg-section">
             <div class="container-fluid px-5">
                 <div class="row">
-                    <div class="col-md-4">
+                    <div class="col-md-4 order-md-1 order-1">
                         <h1 class="primary-text-blue text-center mt-3 fz-70 font-weight-500">Surface Web</h1>
                     </div>
 
-                    <div class="col-md-4 text-center">
+                    <div class="col-md-4 text-center order-md-2 order-3 d-md-block d-none">
                         <img class="img-fluid" src="{{asset('asset_salepage/images/iceberg.png')}}" alt="">
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 order-md-3 order-2">
                         <span class="secondary-text">
                             ตรวจจับการรั่วไหลของข้อมูลที่ถูก Hackerนำไปเผยเแพร่บน Internet จากแหล่งข้อมูลต่างๆ
                         </span>
@@ -149,10 +149,10 @@
 
                 </div>
             </div>
-            <div class="bg-blue-linear">
+            <div class="bg-blue-linear px-5">
                 <div class="container-fluid">
                     <div class="row">
-                        <div class="col-md-4">
+                        <div class="col-md-4 order-md-1 order-2">
                             <span class="secondary-text mb-3 text-white">
                                 ตรวจจับการรั่วไหลของข้อมูลในตลาดมืด (Darkweb) ที่มีการนำข้อมูลในองค์กรมา เผยแพร่หรือจำหน่าย
                             </span>
@@ -171,12 +171,12 @@
                                     <p class="text-white">Finance Info</p>
                                 </li>
                                 <li>
-                                    <img src="{{asset('asset_salepage/images/Group82.png')}}" alt="">
+                                    <img src="{{asset('asset_salepage/images/Group89.png')}}" alt="">
                                     <p class="text-white">Confidential Data</p>
                                 </li>
                             </ul>
                         </div>
-                        <div class="col-md-4 offset-md-4 d-flex align-items-center">
+                        <div class="order-md-2 order-1 col-md-4 offset-md-4 d-flex align-items-center">
                             <h1 class="fz-70 text-white font-weight-500">Dark Web</h1>
                         </div>
                     </div>
@@ -316,7 +316,7 @@
         </div>
     </section>
 
-    <section class="content-section">
+    <section class="content-section bg-c-muted">
         <div class="container">
             <div class="row d-flex align-items-center">
                 <div class="col-lg-6 col-md-6 text-center">
@@ -336,7 +336,7 @@
                                 <p>Fake Mobile <br> Application</p>
                             </li>
                             <li>
-                                <img src="{{asset('asset_salepage/images/Group75.png')}}" alt="">
+                                <img src="{{asset('asset_salepage/images/Group85.png')}}" alt="">
                                 <p>Drive-By <br>Download</p>
                             </li>
                         </ul>
@@ -361,63 +361,33 @@
       Contact Section
     ============================-->
     <section id="contact">
-      <div class="container-fluid">
+      <div class="container">
 
         <div class="section-header">
-          <h3>Contact Us</h3>
+          <h3>Contact</h3>
         </div>
 
         <div class="row wow fadeInUp">
+            <div class="col-lg-6">
+                <div class="contact-container">
+                   <ul class="contact-list">
+                       <li><p class="mb-3"><span class="icon-contact"><i class="fa fa-map-marker"></i></span> 410/ 34 Ratchadaphisek Rd, Khwaeng Samsen Nok, Khet Huai Khwang, Krung Thep Maha Nakhon 10310</p></li>
+                       <li><p class="mb-3"><span class="icon-contact"><i class="fa fa-envelope"></i></span> Support@sosecure.co.th</p></li>
+                       <li><p class="mb-3"><span class="icon-contact"><i class="fa fa-phone"></i></span> 061 564 5294</p></li>
+                       <li><p class="mb-3"><span class="icon-contact"><i class="fab fa-line"></i></span> @sosecure</p></li>
+                   </ul>
 
-          <div class="col-lg-6">
-            <div class="map mb-4 mb-lg-0">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 312px;" allowfullscreen></iframe>
-            </div>
-          </div>
-
-          <div class="col-lg-6">
-            <div class="row">
-              <div class="col-md-5 info">
-                <i class="ion-ios-location-outline"></i>
-                <p>A108 Adam Street, NY 535022</p>
-              </div>
-              <div class="col-md-4 info">
-                <i class="ion-ios-email-outline"></i>
-                <p>info@example.com</p>
-              </div>
-              <div class="col-md-3 info">
-                <i class="ion-ios-telephone-outline"></i>
-                <p>+1 5589 55488 55</p>
-              </div>
+                   <div class="icon-email">
+                        <img src="{{asset('asset_salepage/images/Group25.png')}}" alt="">
+                    </div>
+                </div>
             </div>
 
-            <div class="form">
-              <div id="sendmessage">Your message has been sent. Thank you!</div>
-              <div id="errormessage"></div>
-              <form action="" method="post" role="form" class="contactForm">
-                <div class="form-row">
-                  <div class="form-group col-lg-6">
-                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                    <div class="validation"></div>
-                  </div>
-                  <div class="form-group col-lg-6">
-                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                    <div class="validation"></div>
-                  </div>
+            <div class="col-lg-6">
+                <div class="map mb-4 mb-lg-0">
+                    <iframe src="https://www.google.com/maps?q=410%2F%2034%20Ratchadaphisek%20Rd%2C%20Khwaeng%20Samsen%20Nok%2C%20Khet%20Huai%20Khwang%2C%20Krung%20Thep%20Maha%20Nakhon%2010310&z=14&t=&ie=UTF8&output=embed" frameborder="0" style="border:0; width: 100%; height: 293px;" allowfullscreen></iframe>
                 </div>
-                <div class="form-group">
-                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                  <div class="validation"></div>
-                </div>
-                <div class="form-group">
-                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                  <div class="validation"></div>
-                </div>
-                <div class="text-center"><button type="submit" title="Send Message">Send Message</button></div>
-              </form>
             </div>
-          </div>
-
         </div>
 
       </div>
@@ -429,66 +399,10 @@
     Footer
   ============================-->
   <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-4 col-md-6 footer-info">
-            <h3>NewBiz</h3>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
-          </div>
-
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About us</a></li>
-              <li><a href="#">Services</a></li>
-              <li><a href="#">Terms of service</a></li>
-              <li><a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h4>Contact Us</h4>
-            <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
-
-            <div class="social-links">
-              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
-              <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-            </div>
-
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase seza.</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit"  value="Subscribe">
-            </form>
-          </div>
-
-        </div>
-      </div>
-    </div>
-
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong>NewBiz</strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        &copy; Copyright <strong>Sosecure Threat inSight</strong>. All Rights Reserved
       </div>
     </div>
   </footer>
-  <!-- #footer -->
 @stop
