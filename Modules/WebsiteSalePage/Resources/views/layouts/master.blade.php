b077d0a2d6c9bbcf21848cb1281239f4459f50d8
jamexaoj <jamesicky1@gmail.com>
2021-02-16 09:18:30 +0700

    no message

--- Modules/WebsiteSalePage/Resources/views/layouts/master.blade.php
+++ Modules/WebsiteSalePage/Resources/views/layouts/master.blade.php
@@ -7,9 +7,9 @@
         <title>Intelligence Detection</title>
 
         <link rel="icon" href="{{ asset('images/logo_threat/logo-fav.png')}}" type="image/png">
-       <!-- Google Fonts -->
-       <link rel="preconnect" href="https://fonts.gstatic.com">
-       <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">
 
         <!-- Bootstrap CSS File -->
         <link href="{{ asset('asset_salepage/lib/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
