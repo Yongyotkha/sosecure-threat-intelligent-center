0df339d49858b315245a9bf759987c91d7fa3ffb
jamexaoj <jamesicky1@gmail.com>
2021-02-16 16:27:10 +0700

    no message

--- Modules/SiteSettings/Resources/views/social-datas.blade.php
+++ Modules/SiteSettings/Resources/views/social-datas.blade.php
@@ -379,7 +379,7 @@ function table_social_data(search_val){
             },
             {
                 data: 'content',
-                name: 'content'
+                name: 'content',
             },
             {
                 data: 'data_feed',
@@ -416,7 +416,7 @@ function table_social_data(search_val){
                             console.log(data2);
                             content += feedcontent.replaceAll(data2, '<span class="badge bg-warning">'+data2+'</span>');
                         }
-                        return '<div class="scroll-ovf-content-fixh-60" data-rel="tooltip" title="'+feedcontent+'">'+content+'</div>';
+                        return '<div>'+content+'</div>';
                     }else{
                         return '';
                     }
