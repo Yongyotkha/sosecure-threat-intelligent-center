6772739e7c09956540e96e1d80e97c94ce501ae6
jamexaoj <jamesicky1@gmail.com>
2021-02-17 13:39:52 +0700

    no message

--- Modules/SiteSettings/Resources/views/social-datas_all_site.blade.php
+++ Modules/SiteSettings/Resources/views/social-datas_all_site.blade.php
@@ -254,7 +254,7 @@
                                             <th>Site</th>
                                             <th>Type</th>
                                             <th>Source</th>
-                                            <th>Keyword Ref</th>
+                                            <th>Keyword</th>
                                             <th>Content</th>
                                             <th>Data Feed</th>
                                             <th>View</th>
