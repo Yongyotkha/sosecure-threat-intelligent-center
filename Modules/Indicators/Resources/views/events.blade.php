34d1ac3d5c543e0d46959f362d545405527339b5
S4NTOD3 <todeooooo@gmail.com>
2021-02-20 15:56:42 +0700

    ปรับแก้หน้า indicatore เรื่อง select และย้าย Filter

--- Modules/Indicators/Resources/views/events.blade.php
+++ Modules/Indicators/Resources/views/events.blade.php
@@ -41,88 +41,6 @@
 </header>
 
 <section class="scrollable wrapper">
-    <section id="hide-advance-search" class="panel panel-default" style="display: none;">
-        {{-- <div class="panel-heading">
-            <a class="text-primary" href="{{ route('indicators.events') }}">Events</a>
-            |
-            <a href="{{ route('indicators.attributes') }}" class="text-muted">Attributes</a>
-        </div> --}}
-        <header class="panel-heading font-bold panel-header-blue">
-            <div class="row">
-                <div class="col-md-12">
-                    <i class="fas fa-filter"></i> Filter
-                </div>
-            </header>
-            <div class="panel-body" style="padding: 0 !important">
-                <div class="container-fluid" style="padding: 2rem;">
-                    <div class="row">
-                        <div class="col-lg-4 mb-1">
-                            <h5 class="font-weight-bold">Event Name</h5>
-                            <input type="text" class="form-control" name="event_name" id="event_name"  placeholder="Search">
-                        </div>
-                            <!--<div class="col-md-4">
-                                <div class="form-group">
-                                    <label for="" class="">Group</label>
-                                    {{-- <select name="group[]" id="type" class="select2-option form-control"
-                                        multiple="multiple">
-
-                                    </select> --}}
-                                    <input type="text" class="form-control" name="group" id="group" placeholder="Search">
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label for="" class="">Tag</label>
-                                    {{-- <select name="tag[]" id="tag" class="select2-option form-control" multiple="multiple"> --}}
-                                        <input type="text" class="form-control" name="tag" id="tag" placeholder="Search">
-                                    </select>
-                                </div>
-                            </div>-->
-                            <div class="col-lg-4 mb-1">
-                                <h5 class="font-weight-bold">Date</h5>
-                                <div id="event_date" class="text-center form-control"
-                                style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; display:block;margin-bottom:0;">
-                                <i class="fa fa-calendar"></i>&nbsp;
-                                <span></span> <i class="fa fa-caret-down"></i>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 mb-1">
-                            <h5 class="font-weight-bold">Published</h5>
-                            <div id="groupby-published" class="btn-group special">
-                                <button class="btn btn-grey check_published  active" id="all" value="">
-                                    <span>All</span>
-                                </button>
-                                <button class="btn btn-grey check_published" value="1">
-                                    <span>Published</span>
-                                </button>
-                                <button class="btn btn-grey check_published" value="2">
-                                    <span> No Published </span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="panel-footer">
-                <div class="row">
-                    <div class="col-md-12 text-right">
-                        <button class="btn btn-info" id="btn_search_data">
-                            <i class="fas fa-search btn-fz-13"></i>
-                            <span> @langapp('apply') </span>
-                        </button>
-                        <button class="btn btn-default btn-fz-13" id="btn_reset">
-                            <i class=" fas fa-broom"></i>
-                            <span> Clear </span>
-                        </button>
-                        <button class="btn btn-default btn-fz-13" id="close_filter">
-                            <i class=" fas fa-times"></i>
-                            <span> Close </span>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </section>
-
         <div class="container-fluid" style="margin-bottom:10px;">
             <div class="row">
                 <div class="col-md-4">
@@ -173,6 +91,88 @@
             </div>
         </div>
 
+        <section id="hide-advance-search" class="panel panel-default" style="display: none;">
+            {{-- <div class="panel-heading">
+                <a class="text-primary" href="{{ route('indicators.events') }}">Events</a>
+                |
+                <a href="{{ route('indicators.attributes') }}" class="text-muted">Attributes</a>
+            </div> --}}
+            <header class="panel-heading font-bold panel-header-blue">
+                <div class="row">
+                    <div class="col-md-12">
+                        <i class="fas fa-filter"></i> Filter
+                    </div>
+                </header>
+                <div class="panel-body" style="padding: 0 !important">
+                    <div class="container-fluid" style="padding: 2rem;">
+                        <div class="row">
+                            <div class="col-lg-4 mb-1">
+                                <h5 class="font-weight-bold">Event Name</h5>
+                                <input type="text" class="form-control" name="event_name" id="event_name"  placeholder="Search">
+                            </div>
+                                <!--<div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="" class="">Group</label>
+                                        {{-- <select name="group[]" id="type" class="select2-option form-control"
+                                            multiple="multiple">
+    
+                                        </select> --}}
+                                        <input type="text" class="form-control" name="group" id="group" placeholder="Search">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="" class="">Tag</label>
+                                        {{-- <select name="tag[]" id="tag" class="select2-option form-control" multiple="multiple"> --}}
+                                            <input type="text" class="form-control" name="tag" id="tag" placeholder="Search">
+                                        </select>
+                                    </div>
+                                </div>-->
+                                <div class="col-lg-4 mb-1">
+                                    <h5 class="font-weight-bold">Date</h5>
+                                    <div id="event_date" class="text-center form-control"
+                                    style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; display:block;margin-bottom:0;">
+                                    <i class="fa fa-calendar"></i>&nbsp;
+                                    <span></span> <i class="fa fa-caret-down"></i>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 mb-1">
+                                <h5 class="font-weight-bold">Published</h5>
+                                <div id="groupby-published" class="btn-group special">
+                                    <button class="btn btn-grey check_published  active" id="all" value="">
+                                        <span>All</span>
+                                    </button>
+                                    <button class="btn btn-grey check_published" value="1">
+                                        <span>Published</span>
+                                    </button>
+                                    <button class="btn btn-grey check_published" value="2">
+                                        <span> No Published </span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col-md-12 text-right">
+                            <button class="btn btn-info" id="btn_search_data">
+                                <i class="fas fa-search btn-fz-13"></i>
+                                <span> @langapp('apply') </span>
+                            </button>
+                            <button class="btn btn-default btn-fz-13" id="btn_reset">
+                                <i class=" fas fa-broom"></i>
+                                <span> Clear </span>
+                            </button>
+                            <button class="btn btn-default btn-fz-13" id="close_filter">
+                                <i class=" fas fa-times"></i>
+                                <span> Close </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
         <section class="panel panel-default">
             <header class="panel-heading font-bold panel-header-blue">
                 <div class="row">
@@ -186,7 +186,7 @@
             <div class="panel-body">
 
                 <div class="row m-b-12">
-                    <div class="col-md-6">
+                    <div class="col-md-3">
                         <h5 class="font-weight-bold">Select Industries or Group</h5>
                         <select name="sl_group" id="sl_group" class="form-control sl_group" placeholder="Select">
                             <option value="0" disabled="disabled">Selected</option>
@@ -195,27 +195,36 @@
                             <option value="2">Group</option>
                         </select>
                     </div>
+                    <div class="col-md-9">
+                        <div id="industries_box" style="display: none;">
+                            <h5 class="font-weight-bold">Industries</h5>
+                            <div id="fillter_click" class="button-group">
+                                <span id="btn_industrise"></span>
+                            </div>
+                        </div>
+                        <div id="group_box" class="row" style="display: none;">
+                            <div class="col-md-12">
+                                <h5 class="font-weight-bold">Group</h5>
+                                <div id="fillter_click_group" class="button-group">
+                                    <a class="btn btn-selector active" href="#">Event (0)</a>
+                                    <a class="btn btn-selector" href="#">Attribute (0)</a>
+                                    <a class="btn btn-selector" href="#">OTX (0)</a>
+                                    <a class="btn btn-selector" href="#">MISP (0)</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <div id="industries_box" class="row" style="display: none;">
+                {{-- <div id="industries_box" class="row" style="display: none;">
                     <div class="col-md-12">
                         <h5 class="font-weight-bold">Industries</h5>
                         <div id="fillter_click" class="button-group">
                             <span id="btn_industrise"></span>
                         </div>
                     </div>
-                </div>
-                <div id="group_box" class="row" style="display: none;">
-                    <div class="col-md-12">
-                        <h5 class="font-weight-bold">Group</h5>
-                        <div id="fillter_click_group" class="button-group">
-                            <a class="btn btn-selector active" href="#">Event (0)</a>
-                            <a class="btn btn-selector" href="#">Attribute (0)</a>
-                            <a class="btn btn-selector" href="#">OTX (0)</a>
-                            <a class="btn btn-selector" href="#">MISP (0)</a>
-                        </div>
-                    </div>
-                </div>
+                </div> --}}
+                
 
                 <div class="table-responsive">
                     <table class="table table-striped" id="table_events">
