86173a7fe0b0b914c66347aa0bf1102a3f558083
jamexaoj <jamesicky1@gmail.com>
2021-02-23 15:02:08 +0700

    no message

--- Modules/SiteSettings/Resources/views/darkweb-datas_all_site.blade.php
+++ Modules/SiteSettings/Resources/views/darkweb-datas_all_site.blade.php
@@ -563,7 +563,7 @@
                 serverSide: true,
                 destroy: true,
                 autoWidth:false,
-                "dom": '<"column-xs-flex d-flex justify-content-between m-t-10"l<"d-flex"f<"m-l-10">>>rt<"bottom"ip><"clear">',
+                "dom": '<"column-xs-flex d-flex justify-content-between m-t-10"l<"d-flex"f<"m-l-10"B>>>rt<"bottom"ip><"clear">',
                 ajax: {
                     type: "POST",
                     url: '{!! route('socialdatas.darkweb_all_site_tb') !!}',
