928ee5128446ca2ea38288aad81f54bfcb2ff816
arsarawut <arsarawut.pa@mtsc.co.th>
2021-02-24 18:20:53 +0700

    no message

--- config/cache.php
+++ config/cache.php
@@ -46,8 +46,9 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
+            'path' => 'public/cache_log',//storage_path('framework/cache/data')
             'permission' => 0777,
+
         ],
 
         'memcached' => [
