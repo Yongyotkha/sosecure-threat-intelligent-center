7f49d0ca6b0641f4a3e21b91def725424548a97e
jamexaoj <jamesicky1@gmail.com>
2021-02-24 10:43:37 +0700

    no message

--- Modules/RSSFeedSettings/Resources/views/modal/edit_news.blade.php
+++ Modules/RSSFeedSettings/Resources/views/modal/edit_news.blade.php
@@ -8,12 +8,12 @@
                 <div class="modal-body">
                     <div class="container-fluid">
                         <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-12 m-b-12">
                             <h5>Create News</h5>
                         </div>
                         </div>
                         <div class="form-group row">
-                        <label class="col-lg-12 control-label">Logo </label>
+                        <label class="col-lg-12 control-label m-t-10">Logo </label>
                         <div class="col-lg-12 m-t-10 m-b-12">
                             <div id="area_preview_logo">
                                 <img id="preview-image_logo" src="{{ @$RSSNews -> logo }}"  onerror="setDefaultPic(this)" alt="...">
