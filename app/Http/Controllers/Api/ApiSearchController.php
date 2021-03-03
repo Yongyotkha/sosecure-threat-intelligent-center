8b7c5b014d4b3a9f2048fe1fc20289b7bb24120e
arsarawut <arsarawut.pa@mtsc.co.th>
2021-03-03 16:58:00 +0700

    no message

--- app/Http/Controllers/Api/ApiSearchController.php
+++ app/Http/Controllers/Api/ApiSearchController.php
@@ -173,7 +173,7 @@ class ApiSearchController extends ApiController
                                         '_id' => 0,
                                         'id' => '$pulse_id',
                                         'name' => '$name',
-                                        'content' => [ '$concat' => ['<span class="badge badge-primary">source:</span> ','$source']],
+                                        'content' => [ '$concat' => ['source: ','$source']],
                                         'link' => [ '$concat' => ['/indicators/events/events_detail/','$pulse_id']],
                                     ]
                                 ],
@@ -213,7 +213,7 @@ class ApiSearchController extends ApiController
                                         '_id' => 0,
                                         'id' => '$indicator_id',
                                         'name' => '$indicator',
-                                        'content' => [ '$concat' => ['<span class="badge badge-pill badge-success">type:</span> ', '$type' ]],
+                                        'content' => [ '$concat' => ['type: ', '$type' ]],
                                         'link' => [ '$concat' => ['/indicators/detail?id=','$indicator_id','&type=','$type']],
                                     ]
                                 ],
