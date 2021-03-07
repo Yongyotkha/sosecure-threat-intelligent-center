5df7201659989e27edcdfdd496830ed4f239742f
arsarawut <arsarawut.pa@mtsc.co.th>
2021-03-07 21:20:15 +0700

    no message

--- Modules/WebsiteSalePage/Resources/views/layouts/master.blade.php
+++ Modules/WebsiteSalePage/Resources/views/layouts/master.blade.php
@@ -6,7 +6,29 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Intelligence Detection</title>
 
-        <link rel="icon" href="{{ asset('favicon.ico')}}">
+        {{-- <link rel="icon" href="{{ asset('favicon.ico')}}"> --}}
+
+        <?php $favicon = get_option('site_favicon');
+        $ext = substr($favicon, -4); ?>
+        @if ($ext == '.ico')
+            <link rel="shortcut icon" href="{{ getStorageUrl(config('system.media_dir').'/'.get_option('site_favicon')) }}">
+        @endif
+        @if ($ext == '.png') 
+            <link rel="icon" type="image/png" href="{{ getStorageUrl(config('system.media_dir').'/'.get_option('site_favicon')) }}">
+        @endif
+        @if ($ext == '.jpg' || $ext == 'jpeg') 
+            <link rel="icon" type="image/jpeg" href="{{ getStorageUrl(config('system.media_dir').'/'.get_option('site_favicon')) }}">
+        @endif
+        @if (get_option('site_appleicon') != '')
+            <link rel="apple-touch-icon" href="{{ getStorageUrl(config('system.media_dir').'/'.get_option('site_favicon')) }}"/><!--get_option('site_appleicon') -->
+            <link rel="apple-touch-icon" sizes="72x72"
+                  href="{{ getStorageUrl(config('system.media_dir').'/'.get_option('site_favicon')) }}"/>
+            <link rel="apple-touch-icon" sizes="114x114"
+                  href="{{ getStorageUrl(config('system.media_dir').'/'.get_option('site_favicon')) }}"/>
+            <link rel="apple-touch-icon" sizes="144x144"
+                  href="{{ getStorageUrl(config('system.media_dir').'/'.get_option('site_favicon')) }}"/>
+        @endif
+
         <!-- Google Fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">
