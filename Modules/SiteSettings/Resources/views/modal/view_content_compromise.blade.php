babd6de0c1ba4be2383839c577a2c179a4fdba44
arsarawut <arsarawut.pa@mtsc.co.th>
2021-03-09 10:23:04 +0700

    no message

--- Modules/SiteSettings/Resources/views/modal/view_content_compromise.blade.php
+++ Modules/SiteSettings/Resources/views/modal/view_content_compromise.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-content">
         <div class="modal-header bg-blue">
             <button type="button" class="close text-white" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title text-white">Content Compromise</h4>
+            <h4 class="modal-title text-white"><i class="fas fa-compress fullscreen-btn" onclick="fullscreen();" datdata-rel="tooltip" title="Fullscreen" data-placement="right"></i> Content Compromise</h4>
         </div>
 
         <div class="modal-body">
@@ -20,6 +20,19 @@
     </div>
 </div>
 
+@push('pagestyle')
+{{-- @include('stacks.css.form')
+@include('stacks.css.summernote') --}}
+@endpush
+@push('pagescript')
+{{-- @include('stacks.js.form') --}}
+@include('stacks.js.fullscreen')
+{{-- @include('scripts.summernote')
+@include('stacks.js.markdown') --}}
 <script>
 
 </script>
+@endpush
+
+@stack('pagestyle')
+@stack('pagescript')
--- Modules/SiteSettings/Resources/views/modal/view_content_dataleak.blade.php
+++ Modules/SiteSettings/Resources/views/modal/view_content_dataleak.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-content">
         <div class="modal-header bg-blue">
             <button type="button" class="close text-white" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title text-white">Content Data Leak</h4>
+            <h4 class="modal-title text-white"><i class="fas fa-compress fullscreen-btn" onclick="fullscreen();" datdata-rel="tooltip" title="Fullscreen" data-placement="right"></i> Content Data Leak</h4>
         </div>
 
         <div class="modal-body">
@@ -12,6 +12,8 @@
                         {!!@$feedcontent!!}
                     </h5>
                 </div>
+                <hr>
+                    <div>Activities</div>
             </div>
         </div>
         <div class="modal-footer">
@@ -20,6 +22,19 @@
     </div>
 </div>
 
+@push('pagestyle')
+{{-- @include('stacks.css.form')
+@include('stacks.css.summernote') --}}
+@endpush
+@push('pagescript')
+{{-- @include('stacks.js.form') --}}
+@include('stacks.js.fullscreen')
+{{-- @include('scripts.summernote')
+@include('stacks.js.markdown') --}}
 <script>
 
 </script>
+@endpush
+
+@stack('pagestyle')
+@stack('pagescript')
