524ec8f147f00f0c49a7f4d32135c27c72d08179
arsarawut <arsarawut.pa@mtsc.co.th>
2021-03-09 13:48:04 +0700

    no message

--- routes/api.php
+++ routes/api.php
@@ -109,6 +109,8 @@ Route::group(['prefix' => 'v1/{mode}/{code}'], function () {
     Route::post('web_defacement/update_image', 'Api\ApiWebdefacementController@web_defacement_update_image');
     Route::post('web_defacement/change_status', 'Api\ApiWebdefacementController@web_defacement_change_status');
 
+    Route::post('monitor/monitor_system_save', 'Api\ApiMonitorController@monitor_system_save');
+
     Route::post('check/log_site', 'Api\TransactionLogsite@transaction_log_site');
     
     Route::post('search/searchAll', 'Api\ApiSearchController@searchAll');
