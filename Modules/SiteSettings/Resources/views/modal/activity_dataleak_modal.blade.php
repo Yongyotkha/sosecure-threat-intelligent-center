6863c1dc320c4a8df40d218091efaf9a8fc7c8b9
nontawat_pm <codenon@hotmail.com>
2021-03-09 18:18:52 +0700

    no message

--- Modules/SiteSettings/Resources/views/modal/activity_dataleak_modal.blade.php
+++ Modules/SiteSettings/Resources/views/modal/activity_dataleak_modal.blade.php
@@ -60,7 +60,7 @@
                                             <a href="#gototop" class="btn btn-info btn-xs" onclick="edit_activity('{{addslashes($value->id)}}','{{addslashes($value->title)}}','{{addslashes($value->content)}}');">
                                                 <svg class='svg-inline--fa' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z'></path></svg>
                                             </a>
-                                            <a href="javascript:void(0)" class="btn btn-danger btn-xs" id="asdas" onclick="delete_activity({{$value->code}});"><i class="fas fa-trash-alt"></i></a>
+                                            <a href="javascript:void(0)" class="btn btn-danger btn-xs" id="asdas" onclick="delete_activity('{{addslashes($value->code)}}');"><i class="fas fa-trash-alt"></i></a>
                                         </span>
                                     @endif
                                     
@@ -154,7 +154,6 @@
         $('#textcontent_new').summernote('code',content_activity);
         $("#check_active").val("2");
         $("#id_edited_active").val(id_activity);
-        console.log(id_activity);
     }
 
     function delete_activity(id_activity){
@@ -173,6 +172,7 @@
             }   
         })
     }
+    
 </script>
 @endpush
 
