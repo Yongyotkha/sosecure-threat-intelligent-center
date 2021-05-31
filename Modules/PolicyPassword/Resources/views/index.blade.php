bebe273457933a801c012c415327f746513e4cab
Supakorn <todeooooo@gmail.com>
2021-05-31 17:00:57 +0700

    no message

--- Modules/PolicyPassword/Resources/views/index.blade.php
+++ Modules/PolicyPassword/Resources/views/index.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app2')
 @section('content')
-                           
+<style>
+.bg-danger{
+    background: orangered !important;
+    width:25% !important;
+}  
+.bg-warning{
+    background: orange !important;
+    width:50% !important;
+}  
+.bg-info{
+    background: rgb(0, 174, 255) !important;
+    width:75% !important;
+}  
+.bg-success{
+    background: rgb(21, 255, 0) !important;
+    width:100% !important;
+}  
+</style>            
 <section id="content">
     <section class="vbox">
         <section class="scrollable wrapper bg">
@@ -21,27 +38,27 @@
                             </div>
                             <div class="form-group">
                                 <h4>New Password</h4>
-                                <input type="password" name="" class="form-control form-policy">
+                                <input type="password" id="password" name="" class="form-control form-policy" autocomplete="new-password" onKeyUp="checkPasswordStrength();">
                             </div>
                             <div class="form-group">
                                 <h4>Re-enter Password</h4>
                                 <input type="password" name="" class="form-control form-policy">
                             </div>
 
-                            <div>
+                            <div id="password_st" style="display:none">
                                 <p class="st-pass">Password Strength</p>
-                                <div class="progress-pass">
-                                    <div class="progress-pass-inner" style="background: orangered;width:50%"></div>
-                                    <div class="progress-pass-text">Weak (Should be atleast 8 Characters)</div>
+                                <div class="progress-pass" >
+                                    <div id="password-strength-status" class="progress-pass-inner"></div>
+                                    <div id="password-strength-text" class="progress-pass-text"></div>
                                 </div>
                             </div>
 
                             <ul class="condition-policy">
-                                <li>Be a minimum of 8 characters</li>
-                                <li>Include at least one lowercase letter (a-z)</li>
-                                <li>Include at least one uppercase letter (A-Z)</li>
-                                <li>Include at least one number (0-9)</li>
-                                <li>Include at least one special character</li>
+                                <li class="minimum">Be a minimum of 8 characters</li>
+                                <li class="lowercase">Include at least one lowercase letter (a-z)</li>
+                                <li class="uppercase">Include at least one uppercase letter (A-Z)</li>
+                                <li class="number">Include at least one number (0-9)</li>
+                                <li class="special">Include at least one special character</li>
                             </ul>
 
                             <div class="text-center m-b-xs">
@@ -66,7 +83,108 @@
 @push('pagescript')
 
 <script>
+function checkPasswordStrength() {
+    $('#password_st').show();
+    var number = /([0-9])/;
+    var alphabets = /([a-zA-Z])/;
+    var lowercase = /([a-z])/;
+    var uppercase = /([A-Z])/;
+    var special_characters = /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/;
+    if ($('#password').val().length < 8) {
+        $('#password-strength-status').removeClass();
+        $('#password-strength-text').empty();
+        $('#password-strength-status').addClass('progress-pass-inner bg-danger');
+        $('#password-strength-text').html("Weak");
 
+        $('.minimum').css('color', '');
+        $('.minimum').css('color', 'red');
+
+        if($('#password').val().match(number)){
+            $('.number').css('color', '');
+            $('.number').css('color', 'green');
+        }else{
+            $('.number').css('color', '');
+            $('.number').css('color', 'red');
+        }
+
+        if($('#password').val().match(lowercase)){
+            $('.lowercase').css('color', '');
+            $('.lowercase').css('color', 'green');
+        }else{
+            $('.lowercase').css('color', '');
+            $('.lowercase').css('color', 'red');
+        }
+
+        if($('#password').val().match(uppercase)){
+            $('.uppercase').css('color', '');
+            $('.uppercase').css('color', 'green');
+        }else{
+            $('.uppercase').css('color', '');
+            $('.uppercase').css('color', 'red');
+        }
+
+        if($('#password').val().match(special_characters)){
+            $('.special').css('color', '');
+            $('.special').css('color', 'green');
+        }else{
+            $('.special').css('color', '');
+            $('.special').css('color', 'red');
+        }
+
+    } else {
+        $('.minimum').css('color', '');
+        $('.minimum').css('color', 'green');
+
+        if($('#password').val().match(number)){
+            $('.number').css('color', '');
+            $('.number').css('color', 'green');
+        }else{
+            $('.number').css('color', '');
+            $('.number').css('color', 'red');
+        }
+
+        if($('#password').val().match(lowercase)){
+            $('.lowercase').css('color', '');
+            $('.lowercase').css('color', 'green');
+        }else{
+            $('.lowercase').css('color', '');
+            $('.lowercase').css('color', 'red');
+        }
+
+        if($('#password').val().match(uppercase)){
+            $('.uppercase').css('color', '');
+            $('.uppercase').css('color', 'green');
+        }else{
+            $('.uppercase').css('color', '');
+            $('.uppercase').css('color', 'red');
+        }
+
+        if($('#password').val().match(special_characters)){
+            $('.special').css('color', '');
+            $('.special').css('color', 'green');
+        }else{
+            $('.special').css('color', '');
+            $('.special').css('color', 'red');
+        }
+
+        if ($('#password').val().match(number) && $('#password').val().match(alphabets) && $('#password').val().match(special_characters)) {
+            $('#password-strength-status').removeClass();
+            $('#password-strength-text').empty();
+            $('#password-strength-status').addClass('progress-pass-inner bg-success');
+            $('#password-strength-text').html("Strong");
+        }else if($('#password').val().match(number) && $('#password').val().match(alphabets)){
+            $('#password-strength-status').removeClass();
+            $('#password-strength-text').empty();
+            $('#password-strength-status').addClass('progress-pass-inner bg-info');
+            $('#password-strength-text').html("Medium");
+        }else if ($('#password').val().match(alphabets) || $('#password').val().match(number)) {
+            $('#password-strength-status').removeClass();
+            $('#password-strength-text').empty();
+            $('#password-strength-status').addClass('progress-pass-inner bg-warning');
+            $('#password-strength-text').html("Low");
+        }
+    }
+}
 </script>
 @endpush
 @endsection
