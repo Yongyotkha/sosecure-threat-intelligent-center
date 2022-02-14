0bbeb2ce57dd819edad9aa3cfe988c2d0b48f1aa
jamexaoj <jamesicky1@gmail.com>
2022-02-14 08:56:02 +0700

    no message

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,20 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'UpdateHelper\\' => array($vendorDir . '/kylekatarnls/update-helper/src'),
-    'Twocheckout' => array($vendorDir . '/2checkout/2checkout-php/lib'),
-    'Sentry\\Laravel\\' => array($vendorDir . '/sentry/sentry-laravel/src'),
-    'Sabberworm\\CSS' => array($vendorDir . '/sabberworm/php-css-parser/lib'),
-    'Requests' => array($vendorDir . '/rmccue/requests/library'),
-    'Pimple' => array($vendorDir . '/pimple/pimple/src'),
-    'Parsedown' => array($vendorDir . '/erusev/parsedown'),
-    'OAuth\\Unit' => array($vendorDir . '/lusitanian/oauth/tests'),
-    'OAuth' => array($vendorDir . '/lusitanian/oauth/src'),
-    'Mockery' => array($vendorDir . '/mockery/mockery/library'),
-    'Hautelook' => array($vendorDir . '/hautelook/phpass/src'),
-    'HTMLPurifier' => array($vendorDir . '/ezyang/htmlpurifier/library'),
-    'Eluceo\\iCal' => array($vendorDir . '/eluceo/ical/src'),
-    'Braintree' => array($vendorDir . '/braintree/braintree_php/lib'),
-    'BaconQrCode' => array($vendorDir . '/bacon/bacon-qr-code/src'),
-    'Artdarek\\OAuth' => array($vendorDir . '/oriceon/oauth-5-laravel/src'),
 );
