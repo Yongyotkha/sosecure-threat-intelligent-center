ac20d0d7d3c0364e9b06e1822475223ca7bfddf6
todeooooo <todeooooo@gmail.com>
2022-04-04 23:02:00 +0700

    no message

--- app/Http/Controllers/Api/ApiNewSaveDataController.php
+++ app/Http/Controllers/Api/ApiNewSaveDataController.php
@@ -19,30 +19,32 @@ class ApiNewSaveDataController extends Controller
     }
 
     public function saveSetting(Request $request){
-        $code = $request -> code;
-        $email_alert = $request -> email_alert;
-        $ip = $request -> ip;
-        $protocol = $request -> protocol;
-        $port = $request -> port;
+        try {
+            $code = $request -> code;
 
-        $siteSettings = SiteSettings::where('code', $code)->first();
-        if(!empty($siteSettings)){
-            $siteSettings->server_log_port = trim($request->port);
-            $siteSettings->server_log_protocol = trim($request->protocol);
-            $siteSettings->server_log_ip = trim($request->ip);
-            $siteSettings->save();
-
-            site_config_email_alert::where('site_id', $siteSettings->id)->delete();
-            if ($request->email_alert) {
-                if (count($request->email_alert) > 0) {
-                    foreach ($request->email_alert as $email_alert) {
-                        $site_config_email_alert = new site_config_email_alert;
-                        $site_config_email_alert->site_id = $siteSettings->id;
-                        $site_config_email_alert->email = $email_alert;
-                        $site_config_email_alert->save();
+            $siteSettings = SiteSettings::where('code', $code)->first();
+            if(!empty($siteSettings)){
+                $siteSettings->server_log_port = trim($request->port);
+                $siteSettings->server_log_protocol = trim($request->protocol);
+                $siteSettings->server_log_ip = trim($request->ip);
+                $siteSettings->save();
+    
+                site_config_email_alert::where('site_id', $siteSettings->id)->delete();
+                if (!empty($request->email_alert)) {
+                    if (count($request->email_alert) > 0) {
+                        foreach ($request->email_alert as $email_alert) {
+                            $site_config_email_alert = new site_config_email_alert;
+                            $site_config_email_alert->site_id = $siteSettings->id;
+                            $site_config_email_alert->email = $email_alert;
+                            $site_config_email_alert->save();
+                        }
                     }
                 }
             }
+            return response()->json(['message' => 'Successful', 'error' => '', 'status_code' => '200']);  
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage(), 'error' => $e->getLine(), 'status_code' => '500']);
         }
+    
     }
 }
