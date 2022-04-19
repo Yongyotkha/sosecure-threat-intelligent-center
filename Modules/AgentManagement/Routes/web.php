d7b242268e16bb22dd40829c5e3e5098ffb5fc35
egitoat <egitoat@gmail.com>
2022-04-19 17:17:46 +0700

    no message

--- Modules/AgentManagement/Routes/web.php
+++ Modules/AgentManagement/Routes/web.php
@@ -41,5 +41,6 @@ Route::group(
 		Route::get('/agent_rule_tbl_all_rule', 'AgentManagementController@agent_rule_tbl_all_rule')->name('agentmanagement.agent_rule_tbl_all_rule');
 		Route::post('/agent_rule_insert', 'AgentManagementController@agent_rule_insert')->name('agentmanagement.agent_rule_insert');
 		Route::post('/agent_rule_get_zip', 'AgentManagementController@agent_rule_get_zip')->name('agentmanagement.agent_rule_get_zip');
+		Route::post('/add_new_category', 'AgentManagementController@add_new_category')->name('agentmanagement.add_new_category');
 	}
 );
---
+++ Modules/CompromisedIF/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('compromisedif::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('compromisedif.name') !!}
+    </p>
+@stop
