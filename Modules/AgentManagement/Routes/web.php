ea82f2d2b2b1edc99e6ca6842faf332c9c07dd68
romrun2k <romrun.2k@gmail.com>
2022-04-21 12:12:00 +0700

    no message

--- Modules/AgentManagement/Routes/web.php
+++ Modules/AgentManagement/Routes/web.php
@@ -63,5 +63,7 @@ Route::group(
 		Route::get('/get_rule_site', 'AgentManagementController@get_rule_site')->name('agentmanagement.get_rule_site');
 		Route::post('/check_insert_rule_process', 'AgentManagementController@check_insert_rule_process')->name('agentmanagement.check_insert_rule_process');
 		Route::post('/delete_rule_site', 'AgentManagementController@delete_rule_site')->name('agentmanagement.delete_rule_site');
+	
+		Route::get('/alert_delete', 'AgentManagementController@modal_agent_alert_delete')->name('agentmanagement.modal_agent_alert_delete');
 	}
 );
