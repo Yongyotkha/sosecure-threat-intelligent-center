613127cb2b847a51e43896e69ea2f6108946dc91
egithuboat <108166502+egithuboat@users.noreply.github.com>
2022-07-18 16:37:15 +0700

    no message

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -1016,8 +1016,8 @@ return [
     'actor'                                     => 'Threat Actor',
     'agent_management'                          => 'Agent Management',
     'phishing_detection'                        => 'Phishing Detection',
-    'agent_rule'                        => 'Agent Rule',
-    'host'                        => 'Host',
-    'brand_abuse'                        => 'Brand Abuse',
-    'brand_abuse_feed'                        => 'Brand Abuse Feed',
+    'agent_rule'                                => 'Agent Rule',
+    'host'                                      => 'Hosts',
+    'brand_abuse'                               => 'Brand Abuse',
+    'brand_abuse_feed'                          => 'Brand Abuse Feed',
 ];
