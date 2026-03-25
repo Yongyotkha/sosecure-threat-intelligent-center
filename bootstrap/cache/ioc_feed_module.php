74278052d757d85703646a5d454f9bbd7e232362
thaneejan <thanee.jan@mtsc.co.th>
2026-03-25 16:10:28 +0700

    IOC module

---
+++ bootstrap/cache/ioc_feed_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\IocFeed\\Providers\\IocFeedServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\IocFeed\\Providers\\IocFeedServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
